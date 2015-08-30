7d3c26fbde41d8a4f40b9cc5a5bab24ca3f536ef
Sean Griffin <sean@seantheprogrammer.com>
2015-08-30 14:49:34 -0600

    Make selects in the tests a bit less noisy
    I expect this to be common usage (probably more often on the module
    level than the function level), for any code that is dealing with only
    one table. Even when dealing with multiple tables, the noise will likely
    be reduced a lot with imports and namespaces

--- src/lib.rs
+++ src/lib.rs
@@ -70,15 +70,18 @@ mod test_usage_without_compiler_plugins {
 
     #[test]
     fn with_safe_select() {
+        use self::users::*;
+        use self::users::table as users;
+
         let connection = connection();
         setup_users_table(&connection);
         connection.execute("INSERT INTO users (name) VALUES ('Sean'), ('Tess')")
             .unwrap();
 
-        let select_id = users::table.select(users::id);
+        let select_id = users.select(id);
         // fails to compile (we should test this)
         // let select_id = users::table.select(posts::id);
-        let select_name = users::table.select(users::name);
+        let select_name = users.select(name);
         let ids = connection.query_all(&select_id).unwrap();
         let names: Vec<String> = connection.query_all(&select_name).unwrap();
         // fails to compile (we should test this)
@@ -90,12 +93,15 @@ mod test_usage_without_compiler_plugins {
 
     #[test]
     fn selecting_multiple_columns() {
+        use self::users::*;
+        use self::users::table as users;
+
         let connection = connection();
         setup_users_table(&connection);
         connection.execute("INSERT INTO users (name, age) VALUES ('Jim', 30), ('Bob', 40)")
             .unwrap();
 
-        let source = users::table.select((users::name, users::age));
+        let source = users.select((name, age));
         let expected_data = vec![
             ("Jim".to_string(), Some(30i16)),
             ("Bob".to_string(), Some(40i16)),
