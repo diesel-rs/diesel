mfpiccolo/yaqb

commit a4d4b83b839888940ef2932e51444e3d64d96ad0

Refactor the handling of tuples in `FromSql`
This hides the PG `Row` behind what is essentially an
`Iterator<Item=Any>`. This allows us to significantly improve the code
in the tuple impls of `FromSql`, and completely remove the
`consumed_columns()` nonsense.

With this change, we are assuming (and enforcing, really) that each
column is used only once in the returned data. That was already implied
in the way our types are structured, so this is fine.

src/types/tuples.rs
@@ -1,9 +1,6 @@
-extern crate postgres;
-
-use self::postgres::rows::Row;
-
 use super::{NativeSqlType, FromSql};
 use {Queriable, Table, Column};
+use row::Row;
 
 // FIXME(https://github.com/rust-lang/rust/issues/19630) Remove this work-around
 macro_rules! e {
@@ -23,18 +20,8 @@ macro_rules! tuple_impls {
                 $($ST: NativeSqlType),+
             {
                 #[allow(unused_assignments)]
-                fn from_sql(row: &Row, mut idx: usize) -> Self {
-                    (
-                        $({
-                            let value = $T::from_sql(row, idx);
-                            idx += $T::consumed_columns();
-                            value
-                        }),+
-                    )
-                }
-
-                fn consumed_columns() -> usize {
-                    $($T::consumed_columns() + )* 0
+                fn from_sql<T: Row>(row: &mut T) -> Self {
+                    ($($T::from_sql(row)),+)
                 }
             }
 

src/connection/mod.rs
@@ -1,8 +1,8 @@
 extern crate postgres;
 
-use super::query_source::{Queriable, QuerySource};
-use super::{Result, ConnectionResult};
-use super::types::FromSql;
+use {Result, ConnectionResult, QuerySource, Queriable};
+use types::FromSql;
+use row::PgRow;
 use self::postgres::{SslMode, Statement};
 
 pub struct Connection {
@@ -29,7 +29,7 @@ impl Connection {
         let stmt = try!(self.prepare_query(source));
         let rows = try!(stmt.query(&[]));
         Ok(rows.into_iter().map(|row| {
-            let values = U::Row::from_sql(&row, 0);
+            let values = U::Row::from_sql(&mut PgRow::wrap(row));
             U::build(values)
         }).nth(0))
     }
@@ -41,7 +41,7 @@ impl Connection {
         let stmt = try!(self.prepare_query(source));
         let rows = try!(stmt.query(&[]));
         let result: Vec<U> = rows.into_iter().map(|row| {
-            let values = U::Row::from_sql(&row, 0);
+            let values = U::Row::from_sql(&mut PgRow::wrap(row));
             U::build(values)
         }).collect();
         Ok(Cursor(result.into_iter()))

src/lib.rs
@@ -1,8 +1,10 @@
 pub mod types;
 
-mod query_source;
 mod connection;
+mod query_source;
 mod result;
+mod row;
+
 #[macro_use]
 mod macros;
 

src/row.rs
@@ -1,0 +1,30 @@
+extern crate postgres;
+
+use self::postgres::rows::Row as PgInnerRow;
+use self::postgres::types::FromSql as PgFromSql;
+
+pub trait Row {
+    fn take<T: PgFromSql>(&mut self) -> T;
+}
+
+pub struct PgRow<'a> {
+    inner: PgInnerRow<'a>,
+    idx: usize,
+}
+
+impl<'a> PgRow<'a> {
+    pub fn wrap(inner: PgInnerRow<'a>) -> Self {
+        PgRow {
+            inner: inner,
+            idx: 0,
+        }
+    }
+}
+
+impl<'a> Row for PgRow<'a> {
+    fn take<T: PgFromSql>(&mut self) -> T {
+        let current_idx = self.idx;
+        self.idx += 1;
+        self.inner.get(current_idx)
+    }
+}

src/types/mod.rs
@@ -1,9 +1,7 @@
 mod tuples;
 mod primitives;
 
-extern crate postgres;
-
-use self::postgres::rows::Row;
+use row::Row;
 
 pub struct Bool;
 
@@ -27,9 +25,5 @@ pub struct Nullable<T: NativeSqlType>(T);
 pub trait NativeSqlType {}
 
 pub trait FromSql<A: NativeSqlType> {
-    fn from_sql(row: &Row, idx: usize) -> Self;
-
-    fn consumed_columns() -> usize {
-        1
-    }
+    fn from_sql<T: Row>(row: &mut T) -> Self;
 }

src/types/primitives.rs
@@ -1,18 +1,18 @@
 extern crate postgres;
 
-use self::postgres::rows::Row;
 use self::postgres::types::FromSql as NativeFromSql;
 
 use super::{NativeSqlType, FromSql, Nullable};
 use Queriable;
+use row::Row;
 
 macro_rules! primitive_impls {
     ($($Source:ident -> $Target:ty),+,) => {
         $(
             impl NativeSqlType for super::$Source {}
             impl FromSql<super::$Source> for $Target {
-                fn from_sql(row: &Row, idx: usize) -> Self {
-                    row.get(idx)
+                fn from_sql<T: Row>(row: &mut T) -> Self {
+                    row.take()
                 }
             }
 
@@ -51,7 +51,7 @@ impl<T, ST> FromSql<Nullable<ST>> for Option<T> where
     T: FromSql<ST> + NativeFromSql,
     ST: NativeSqlType,
 {
-    fn from_sql(row: &Row, idx: usize) -> Self {
-        row.get(idx)
+    fn from_sql<R: Row>(row: &mut R) -> Self {
+        row.take()
     }
 }

diesel-rs/diesel

commit 08ea755fc65e775de28a6b0900eb7b7a66bd5e47

Remove an unnecessary lint skip
We refactored out the code that caused the original warning in
a4d4b83b839888940ef2932e51444e3d64d96ad0

src/types/tuples.rs
@@ -21,7 +21,6 @@ macro_rules! tuple_impls {
                 $($T: FromSql<$ST>),+,
                 $($ST: NativeSqlType),+
             {
-                #[allow(unused_assignments)]
                 fn from_sql<T: Row>(row: &mut T) -> Self {
                     ($($T::from_sql(row)),+)
                 }