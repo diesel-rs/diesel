d14143ead6d2b3eab7b43471e7e0e8293bda741c
Sean Griffin <sean@seantheprogrammer.com>
2015-09-14 11:31:42 +0100

    Add a test for defaults that are never provided
    I was going to use this to start to spec out how we are going to handle
    timestamps, but I couldn't figure out what type to use in Rust to
    represent the times.

--- src/lib.rs
+++ src/lib.rs
@@ -411,9 +411,38 @@ mod test_usage_without_compiler_plugins {
         assert_eq!(expected_users, inserted_users);
     }
 
+    #[test]
+    fn insert_with_defaults_not_provided() {
+        use self::users::table as users;
+        let connection = connection();
+        connection.execute("CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            hair_color VARCHAR NOT NULL DEFAULT 'Green'
+        )").unwrap();
+        let new_users = vec![
+            BaldUser { name: "Sean".to_string() },
+            BaldUser { name: "Tess".to_string() },
+        ];
+        let inserted_users: Vec<_> = connection.insert(&users, new_users).unwrap().collect();
+
+        let expected_users = vec![
+            User { id: 1, name: "Sean".to_string(), hair_color: Some("Green".to_string()) },
+            User { id: 2, name: "Tess".to_string(), hair_color: Some("Green".to_string()) },
+        ];
+        let actual_users: Vec<_> = connection.query_all(&users).unwrap().collect();
+
+        assert_eq!(expected_users, actual_users);
+        assert_eq!(expected_users, inserted_users);
+    }
+
     struct NewUser {
         name: String,
         hair_color: Option<String>,
+    }
+
+    struct BaldUser {
+        name: String,
     }
 
     impl NewUser {
@@ -429,6 +458,12 @@ mod test_usage_without_compiler_plugins {
         NewUser -> users {
             name -> String,
             hair_color -> Option<String>,
+        }
+    }
+
+    insertable! {
+        BaldUser -> users {
+            name -> String,
         }
     }
 
