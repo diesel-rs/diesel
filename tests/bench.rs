06589d830de4a2e1b77aafb07be80afbe21ab652
Sean Griffin <sean@seantheprogrammer.com>
2015-10-03 08:58:37 -0400

    Add a basic micro-benchmark
    I'm about to make a change that I'm curious about the performance
    implications of. This will allow me to track it.

---
+++ tests/bench.rs
@@ -0,0 +1,23 @@
+#![feature(test)]
+#[macro_use]
+extern crate yaqb;
+extern crate test;
+
+mod schema;
+
+use self::test::Bencher;
+use self::schema::*;
+
+#[bench]
+fn bench_selecting_10k_rows(b: &mut Bencher) {
+    let conn = connection();
+    setup_users_table(&conn);
+    let data = (0..10_000).map(|i| {
+        NewUser::new(&format!("User {}", i), None)
+    }).collect();
+    conn.insert_without_return(&users::table, data).unwrap();
+
+    b.iter(|| {
+        conn.query_all(&users::table).unwrap().collect::<Vec<User>>()
+    })
+}
