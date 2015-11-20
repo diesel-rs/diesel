c19cb890b7cf25615bf9599847d080104f023e0c
Sean Griffin <sean@seantheprogrammer.com>
2015-11-20 14:27:21 -0700

    Add a few additional benchmarks

--- tests/bench.rs
+++ tests/bench.rs
@@ -7,9 +7,20 @@ mod schema;
 
 use self::test::Bencher;
 use self::schema::*;
+use yaqb::*;
 
 #[bench]
-fn bench_selecting_10k_rows(b: &mut Bencher) {
+fn bench_selecting_0_rows_with_trivial_query(b: &mut Bencher) {
+    let conn = connection();
+    setup_users_table(&conn);
+
+    b.iter(|| {
+        conn.query_all(users::table).unwrap().collect::<Vec<User>>()
+    })
+}
+
+#[bench]
+fn bench_selecting_10k_rows_with_trivial_query(b: &mut Bencher) {
     let conn = connection();
     setup_users_table(&conn);
     let data: Vec<_> = (0..10_000).map(|i| {
@@ -19,5 +30,41 @@ fn bench_selecting_10k_rows(b: &mut Bencher) {
 
     b.iter(|| {
         conn.query_all(users::table).unwrap().collect::<Vec<User>>()
+    })
+}
+
+#[bench]
+fn bench_selecting_0_rows_with_medium_complex_query(b: &mut Bencher) {
+    let conn = connection();
+    setup_users_table(&conn);
+    setup_posts_table(&conn);
+
+    b.iter(|| {
+        use schema::users::dsl::*;
+        let target = users.left_outer_join(posts::table)
+            .filter(hair_color.eq("black"))
+            .order(name.desc());
+        conn.query_all(target).unwrap().collect::<Vec<(User, Option<Post>)>>()
+    })
+}
+
+#[bench]
+fn bench_selecting_10k_rows_with_medium_complex_query(b: &mut Bencher) {
+    let conn = connection();
+    setup_users_table(&conn);
+    setup_posts_table(&conn);
+
+    let data: Vec<_> = (0..10_000).map(|i| {
+        let hair_color = if i % 2 == 0 { "black" } else { "brown" };
+        NewUser::new(&format!("User {}", i), Some(hair_color))
+    }).collect();
+    conn.insert_returning_count(&users::table, &data).unwrap();
+
+    b.iter(|| {
+        use schema::users::dsl::*;
+        let target = users.left_outer_join(posts::table)
+            .filter(hair_color.eq("black"))
+            .order(name.desc());
+        conn.query_all(target).unwrap().collect::<Vec<(User, Option<Post>)>>()
     })
 }
