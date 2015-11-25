8e5b26d51d20e5e1f02a71326bd6565ab967644f
Sean Griffin <sean@seantheprogrammer.com>
2015-11-25 10:58:04 -0700

    Allow `infix_predicate` to be used from other crates
    I've also unlocked the return type so it can be used for things like
    the concat operator on `tsvector`, which returns a new `tsvector`. Maybe
    `infix_operator` woulf be a better name for this macro

--- src/expression/predicates.rs
+++ src/expression/predicates.rs
@@ -1,9 +1,10 @@
-use query_builder::*;
-use super::{Expression, SelectableExpression, NonAggregate};
-use types::Bool;
-
+#[macro_export]
 macro_rules! infix_predicate {
     ($name:ident, $operator:expr) => {
+        infix_predicate!($name, $operator, $crate::types::Bool);
+    };
+
+    ($name:ident, $operator:expr, $return_type:ty) => {
         #[derive(Debug, Clone, Copy)]
         pub struct $name<T, U> {
             left: T,
@@ -19,28 +20,28 @@ macro_rules! infix_predicate {
             }
         }
 
-        impl<T, U> Expression for $name<T, U> where
-            T: Expression,
-            U: Expression,
+        impl<T, U> $crate::expression::Expression for $name<T, U> where
+            T: $crate::expression::Expression,
+            U: $crate::expression::Expression,
         {
-            type SqlType = Bool;
+            type SqlType = $return_type;
 
-            fn to_sql(&self, out: &mut QueryBuilder) -> BuildQueryResult {
+            fn to_sql(&self, out: &mut $crate::query_builder::QueryBuilder) -> $crate::query_builder::BuildQueryResult {
                 try!(self.left.to_sql(out));
                 out.push_sql($operator);
                 self.right.to_sql(out)
             }
         }
 
-        impl<T, U, QS> SelectableExpression<QS> for $name<T, U> where
-            T: SelectableExpression<QS>,
-            U: SelectableExpression<QS>,
+        impl<T, U, QS> $crate::expression::SelectableExpression<QS> for $name<T, U> where
+            T: $crate::expression::SelectableExpression<QS>,
+            U: $crate::expression::SelectableExpression<QS>,
         {
         }
 
-        impl<T, U> NonAggregate for $name<T, U> where
-            T: NonAggregate,
-            U: NonAggregate,
+        impl<T, U> $crate::expression::NonAggregate for $name<T, U> where
+            T: $crate::expression::NonAggregate,
+            U: $crate::expression::NonAggregate,
         {
         }
     }
@@ -60,6 +61,8 @@ infix_predicate!(NotLike, " NOT LIKE ");
 infix_predicate!(Or, " OR ");
 
 use query_source::Column;
+use query_builder::*;
+use super::{Expression, SelectableExpression};
 
 impl<T, U> Changeset for Eq<T, U> where
     T: Column,
