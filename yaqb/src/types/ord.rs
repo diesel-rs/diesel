eece68b15d01f1e1ef53605bacaa58da80da7288
Sean Griffin <sean@seantheprogrammer.com>
2015-11-29 10:14:06 -0700

    Implement `SqlOrd` for date and time types
    I missed this when I added them, but they can definitely be ordered.

--- yaqb/src/types/ord.rs
+++ yaqb/src/types/ord.rs
@@ -9,4 +9,8 @@ impl SqlOrd for types::Float {}
 impl SqlOrd for types::Double {}
 impl SqlOrd for types::VarChar {}
 impl SqlOrd for types::Text {}
+impl SqlOrd for types::Date {}
+impl SqlOrd for types::Interval {}
+impl SqlOrd for types::Time {}
+impl SqlOrd for types::Timestamp {}
 impl<T: SqlOrd + NativeSqlType> SqlOrd for types::Nullable<T> {}
