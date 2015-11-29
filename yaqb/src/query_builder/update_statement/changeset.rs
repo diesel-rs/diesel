a19f7126b5452a9185da747b960225863b94f733
Sean Griffin <sean@seantheprogrammer.com>
2015-11-28 18:03:20 -0700

    Implement `Changeset` for `Vec<T>` and `Box<T>`
    A valid use case has arisen in #26 that is impossible to work around at
    the moment. We're going to eventually add a standard API to handle this,
    so this implementation has no tests. It will get tested through that
    API, once we've figured out what the API will look like. But I want to
    get this through so it's possible to work around this by implementing
    `AsChangeset` manually.

--- yaqb/src/query_builder/update_statement/changeset.rs
+++ yaqb/src/query_builder/update_statement/changeset.rs
@@ -22,3 +22,25 @@ impl<T> AsChangeset for T where
         self
     }
 }
+
+impl<T: Changeset> Changeset for Vec<T> {
+    type Target = T::Target;
+
+    fn to_sql(&self, out: &mut QueryBuilder) -> BuildQueryResult {
+        for (i, changeset) in self.iter().enumerate() {
+            if i != 0 {
+                out.push_sql(", ");
+            }
+            try!(changeset.to_sql(out))
+        }
+        Ok(())
+    }
+}
+
+impl<T: Changeset> Changeset for Box<T> {
+    type Target = T::Target;
+
+    fn to_sql(&self, out: &mut QueryBuilder) -> BuildQueryResult {
+        (&**self).to_sql(out)
+    }
+}
