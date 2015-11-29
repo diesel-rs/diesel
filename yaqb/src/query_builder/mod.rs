bef7e7261d33113aee19b485b4e3d49fba653ab9
Sean Griffin <sean@seantheprogrammer.com>
2015-11-29 09:18:57 -0700

    Document the top level `query_builder` module
    Writing this got me thinking about some API changes for consistency.
    See #29 and #30.

--- yaqb/src/query_builder/mod.rs
+++ yaqb/src/query_builder/mod.rs
@@ -1,3 +1,4 @@
+//! Contains traits responsible for the actual construction of SQL statements
 #[doc(hidden)]
 pub mod pg;
 
@@ -6,6 +7,7 @@ mod offset_clause;
 mod order_clause;
 mod select_statement;
 mod where_clause;
+#[doc(hidden)]
 pub mod update_statement;
 mod delete_statement;
 mod functions;
@@ -13,21 +15,33 @@ mod functions;
 pub use self::functions::*;
 #[doc(hidden)]
 pub use self::select_statement::SelectStatement;
+#[doc(inline)]
 pub use self::update_statement::{IncompleteUpdateStatement, AsChangeset, Changeset, UpdateTarget};
 
 use expression::Expression;
 use std::error::Error;
 use types::NativeSqlType;
 
+#[doc(hidden)]
 pub type Binds = Vec<Option<Vec<u8>>>;
 pub type BuildQueryResult = Result<(), Box<Error>>;
 
+/// Apps should not need to concern themselves with this trait.
+///
+/// This is the trait used to actually construct a SQL query. You will take one
+/// of these as an argument if you're implementing
+/// [`Expression`](../expression/trait.Expression.html) or
+/// [`QueryFragment`](trait.QueryFragment.html) manually.
 pub trait QueryBuilder {
     fn push_sql(&mut self, sql: &str);
     fn push_identifier(&mut self, identifier: &str) -> BuildQueryResult;
     fn push_bound_value(&mut self, tpe: &NativeSqlType, binds: Option<Vec<u8>>);
 }
 
+/// A complete SQL query with a return type. This can be a select statement, or
+/// a command such as `update` or `insert` with a `RETURNING` clause. Unlike
+/// [`Expression`](../expression/trait.Expression.html), types implementing this
+/// trait are guaranteed to be executable on their own.
 pub trait Query: QueryFragment {
     type SqlType: NativeSqlType;
 }
@@ -36,6 +50,11 @@ impl<'a, T: Query> Query for &'a T where &'a T: QueryFragment {
     type SqlType = T::SqlType;
 }
 
+/// An untyped fragment of SQL. This may be a complete SQL command (such as
+/// an update statement without a `RETURNING` clause), or a subsection (such as
+/// our internal types used to represent a `WHERE` clause). All methods on
+/// [`Connection`](../struct.Connection.html) that execute a query require this
+/// trait to be implemented.
 pub trait QueryFragment {
     fn to_sql(&self, out: &mut QueryBuilder) -> BuildQueryResult;
 }
@@ -46,6 +65,9 @@ impl<T: Expression> QueryFragment for T {
     }
 }
 
+/// Types that can be converted into a complete, typed SQL query. This is used
+/// internally to automatically add the right select clause when none is
+/// specified, or to automatically add `RETURNING *` in certain contexts
 pub trait AsQuery {
     type SqlType: NativeSqlType;
     type Query: Query<SqlType=Self::SqlType>;
