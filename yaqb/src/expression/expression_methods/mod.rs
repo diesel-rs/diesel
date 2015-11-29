6cd41bd703d7247b7f1d2287ab54f1097b746056
Sean Griffin <sean@seantheprogrammer.com>
2015-11-28 18:11:18 -0700

    Ensure the expression method traits appear in the module level docs

--- yaqb/src/expression/expression_methods/mod.rs
+++ yaqb/src/expression/expression_methods/mod.rs
@@ -1,7 +1,10 @@
+#[doc(hidden)]
 pub mod global_expression_methods;
+#[doc(hidden)]
 pub mod bool_expression_methods;
+#[doc(hidden)]
 pub mod text_expression_methods;
 
-pub use self::global_expression_methods::*;
-pub use self::bool_expression_methods::*;
-pub use self::text_expression_methods::*;
+pub use self::global_expression_methods::ExpressionMethods;
+pub use self::bool_expression_methods::BoolExpressionMethods;
+pub use self::text_expression_methods::{TextExpressionMethods, VarCharExpressionMethods};
