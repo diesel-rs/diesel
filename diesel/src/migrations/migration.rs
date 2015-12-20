d01026f66835b8151e82043e070dbf9647d93149
Sean Griffin <sean@seantheprogrammer.com>
2015-12-20 09:45:44 -0700

    Allow migrations to have more meaningful names
    We don't just want to have a bunch of directories with meaningless
    timestamps, as it becomes difficult to know what to edit/read. This also
    backfills some missing tests for pieces which can be tested in isolation.

--- diesel/src/migrations/migration.rs
+++ diesel/src/migrations/migration.rs
@@ -35,11 +35,14 @@ struct SqlFileMigration(PathBuf);
 
 impl Migration for SqlFileMigration {
     fn version(&self) -> String {
-        self.0.components().last().unwrap()
-            .as_ref()
+        self.0.file_name().unwrap()
             .to_os_string()
             .into_string()
             .unwrap()
+            .split("_")
+            .nth(0)
+            .unwrap()
+            .to_string()
     }
 
     fn run(&self, conn: &Connection) -> Result<(), RunMigrationsError> {
@@ -57,4 +60,28 @@ fn run_sql_from_file(conn: &Connection, path: &Path) -> Result<(), RunMigrations
     try!(file.read_to_string(&mut sql));
     try!(conn.batch_execute(&sql));
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SqlFileMigration;
+    use super::*;
+
+    use std::path::PathBuf;
+
+    #[test]
+    fn sql_file_migration_version_is_based_on_folder_name() {
+        let path = PathBuf::new().join("migrations").join("12345");
+        let migration = SqlFileMigration(path);
+
+        assert_eq!("12345", migration.version());
+    }
+
+    #[test]
+    fn sql_file_migration_version_allows_additional_naming() {
+        let path = PathBuf::new().join("migrations").join("54321_create_stuff");
+        let migration = SqlFileMigration(path);
+
+        assert_eq!("54321", migration.version());
+    }
 }
