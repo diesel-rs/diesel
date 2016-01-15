c0b0fb8c50908c5d9ca2cbf8853059e1cbb6b6d1
Sean Griffin <sean@seantheprogrammer.com>
2016-01-15 09:43:09 -0700

    Add support for `chrono::NaiveDate`
    We end up with an incredibly strange subset of supported ranges. PG
    supports anything that can be represented as julian days using an `i32`
    (why this is signed, I do not know. It does not support negative
    values). Chrono is representing its days as... something... The range it
    supports covers 191492259, so I have no clue what it's representation
    is. Anyway, PG can't go earlier than 4913 BCE, and chrono can't go later
    than 262143 CE. So that's the range we support. My apologies to all the
    cave men, and people in the distant future who we are not supporting.
    Hopefully this will be updated at some point in the next 260,000 years.

--- diesel/src/types/impls/date_and_time/chrono.rs
+++ diesel/src/types/impls/date_and_time/chrono.rs
@@ -5,36 +5,66 @@ extern crate chrono;
 use std::error::Error;
 use std::io::Write;
 use self::chrono::{Duration, NaiveDateTime, NaiveDate};
+use self::chrono::naive::date;
 
 use expression::AsExpression;
 use expression::bound::Bound;
 use query_source::Queriable;
-use super::PgTimestamp;
-use types::{self, FromSql, IsNull, Timestamp, ToSql};
+use super::{PgDate, PgTimestamp};
+use types::{self, FromSql, IsNull, Timestamp, Date, ToSql};
 
 expression_impls! {
+    Date -> NaiveDate,
     Timestamp -> NaiveDateTime,
 }
 
 queriable_impls! {
+    Date -> NaiveDate,
     Timestamp -> NaiveDateTime,
 }
 
-// Postgres timestamps start from January 1st 2000.
-fn pg_epoch() -> NaiveDateTime {
-    NaiveDate::from_ymd(2000, 1, 1).and_hms(0, 0, 0)
+fn pg_epoch() -> NaiveDate {
+    NaiveDate::from_ymd(2000, 1, 1)
+}
+
+fn julian_epoch() -> NaiveDate {
+    NaiveDate::from_ymd(-4713, 1, 1)
+}
+
+impl FromSql<Date> for NaiveDate {
+    fn from_sql(bytes: Option<&[u8]>) -> Result<Self, Box<Error>> {
+        let PgDate(offset) = try!(FromSql::<Date>::from_sql(bytes));
+        match pg_epoch().checked_add(Duration::days(offset as i64)) {
+            Some(date) => Ok(date),
+            None => {
+                let error_message = format!("Chrono can only represent dates up to {:?}", date::MAX);
+                Err(Box::<Error + Send + Sync>::from(error_message))
+            }
+        }
+    }
 }
 
 impl FromSql<Timestamp> for NaiveDateTime {
     fn from_sql(bytes: Option<&[u8]>) -> Result<Self, Box<Error>> {
         let PgTimestamp(offset) = try!(FromSql::<Timestamp>::from_sql(bytes));
-        Ok(pg_epoch() + Duration::microseconds(offset))
+        Ok(pg_epoch().and_hms(0, 0, 0) + Duration::microseconds(offset))
+    }
+}
+
+impl ToSql<Date> for NaiveDate {
+    fn to_sql<W: Write>(&self, out: &mut W) -> Result<IsNull, Box<Error>> {
+        if *self < julian_epoch() {
+            let error_message = format!("Cannot store dates earlier than {:?}", julian_epoch());
+            return Err(Box::<Error + Send + Sync>::from(error_message));
+        };
+        let days_since_epoch = (*self - pg_epoch()).num_days();
+        ToSql::<Date>::to_sql(&PgDate(days_since_epoch as i32), out)
     }
 }
 
 impl ToSql<Timestamp> for NaiveDateTime {
     fn to_sql<W: Write>(&self, out: &mut W) -> Result<IsNull, Box<Error>> {
-        let time = match (*self - pg_epoch()).num_microseconds() {
+        let time = match (*self - pg_epoch().and_hms(0, 0, 0)).num_microseconds() {
             Some(time) => time,
             None => {
                 let error_message = format!("{:?} as microseconds is too large to fit in an i64", self);
