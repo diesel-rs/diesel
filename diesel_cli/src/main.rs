7a39a8c205974af36f04b908d5d428d794dc4c6d
Cameron Alexander <emptyflash@gmail.com>
2016-02-03 11:44:05 -0600

    Removed internal copy of Path#strip_prefix

--- diesel_cli/src/main.rs
+++ diesel_cli/src/main.rs
@@ -286,37 +286,8 @@ fn convert_absolute_path_to_relative(target_path: &mut PathBuf, current_path: &m
         current_path = current_path.parent().unwrap();
     }
 
-    result.join(strip_prefix(target_path, current_path))
+    result.join(target_path.strip_prefix(current_path).unwrap())
 }
-
-// FIXME: Remove all of this when 1.7 is stable
-fn strip_prefix<'a>(target: &'a Path, base: &'a Path)
--> &'a Path {
-    iter_after(target.components(), base.components())
-        .map(|c| c.as_path()).unwrap()
-}
-
-fn iter_after<A, I, J>(mut iter: I, mut prefix: J) -> Option<I>
-where I: Iterator<Item = A> + Clone,
-      J: Iterator<Item = A>,
-      A: PartialEq
-{
-    loop {
-        let mut iter_next = iter.clone();
-        match (iter_next.next(), prefix.next()) {
-            (Some(x), Some(y)) => {
-                if x != y {
-                    return None;
-                }
-            }
-            (Some(_), None) => return Some(iter),
-            (None, None) => return Some(iter),
-            (None, Some(_)) => return None,
-        }
-        iter = iter_next;
-    }
-}
-// End FIXME
 
 #[cfg(test)]
 mod tests {
