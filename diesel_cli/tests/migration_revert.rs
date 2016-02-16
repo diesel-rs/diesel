262be91e71a6976b9c1c8cd82ad234fec49775ce
Matt Casper <matthewvcasper@gmail.com>
2016-02-15 16:12:30 -0800

    Add tests for `diesel migration revert`

--- diesel_cli/tests/migration_revert.rs
+++ diesel_cli/tests/migration_revert.rs
@@ -1,10 +1,12 @@
-use support::project;
+use support::{database, project};
 
 #[test]
-fn migration_redo_runs_the_last_migration_down_and_up() {
-    let p = project("migration_redo")
+fn migration_revert_runs_the_last_migration_down() {
+    let p = project("migration_revert")
         .folder("migrations")
         .build();
+    let db = database(&p.database_url());
+
     p.create_migration("12345_create_users_table",
                        "CREATE TABLE users ( id INTEGER )",
                        "DROP TABLE users");
@@ -12,16 +14,14 @@ fn migration_redo_runs_the_last_migration_down_and_up() {
     // Make sure the project is setup
     p.command("setup").run();
 
+    assert!(db.table_exists("users"));
+
     let result = p.command("migration")
-        .arg("redo")
+        .arg("revert")
         .run();
 
-    let expected_stdout = "\
-Rolling back migration 12345
-Running migration 12345
-";
-
     assert!(result.is_success(), "Result was unsuccessful {:?}", result);
-    assert!(result.stdout().contains(expected_stdout),
+    assert!(result.stdout().contains("Rolling back migration 12345"),
         "Unexpected stdout {}", result.stdout());
+    assert!(!db.table_exists("users"));
 }
--- diesel_cli/tests/tests.rs
+++ diesel_cli/tests/tests.rs
@@ -6,5 +6,6 @@ mod setup;
 mod support;
 mod migration_generate;
 mod migration_redo;
+mod migration_revert;
 mod database_setup;
 mod database_reset;
