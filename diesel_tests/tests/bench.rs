1e5d1c0f0ef1f2d3776d5e3557b58e4a600bfc2e
Sean Griffin <sean@seantheprogrammer.com>
2016-03-28 13:23:43 -0600

    Update benchmarks to account for recent changes

--- diesel_tests/tests/bench.rs
+++ diesel_tests/tests/bench.rs
@@ -15,7 +15,7 @@ fn bench_selecting_0_rows_with_trivial_query(b: &mut Bencher) {
     let conn = connection();
 
     b.iter(|| {
-        users::table.load(&conn).unwrap().collect::<Vec<User>>();
+        users::table.load::<User>(&conn).unwrap();
     })
 }
 
@@ -28,9 +28,23 @@ fn bench_selecting_10k_rows_with_trivial_query(b: &mut Bencher) {
     batch_insert(&data, users::table, &conn);
 
     b.iter(|| {
-        users::table.load(&conn).unwrap().collect::<Vec<User>>()
+        users::table.load::<User>(&conn).unwrap()
     })
 }
+
+#[bench]
+fn bench_selecting_10k_rows_with_trivial_query_boxed(b: &mut Bencher) {
+    let conn = connection();
+    let data: Vec<_> = (0..10_000).map(|i| {
+        NewUser::new(&format!("User {}", i), None)
+    }).collect();
+    batch_insert(&data, users::table, &conn);
+
+    b.iter(|| {
+        users::table.into_boxed().load::<User>(&conn).unwrap()
+    })
+}
+
 
 #[bench]
 fn bench_selecting_0_rows_with_medium_complex_query(b: &mut Bencher) {
@@ -41,7 +55,7 @@ fn bench_selecting_0_rows_with_medium_complex_query(b: &mut Bencher) {
         let target = users.left_outer_join(posts::table)
             .filter(hair_color.eq("black"))
             .order(name.desc());
-        target.load(&conn).unwrap().collect::<Vec<(User, Option<Post>)>>()
+        target.load::<(User, Option<Post>)>(&conn).unwrap()
     })
 }
 
@@ -60,6 +74,26 @@ fn bench_selecting_10k_rows_with_medium_complex_query(b: &mut Bencher) {
         let target = users.left_outer_join(posts::table)
             .filter(hair_color.eq("black"))
             .order(name.desc());
-        target.load(&conn).unwrap().collect::<Vec<(User, Option<Post>)>>()
+        target.load::<(User, Option<Post>)>(&conn).unwrap()
+    })
+}
+
+#[bench]
+fn bench_selecting_10k_rows_with_medium_complex_query_boxed(b: &mut Bencher) {
+    let conn = connection();
+
+    let data: Vec<_> = (0..10_000).map(|i| {
+        let hair_color = if i % 2 == 0 { "black" } else { "brown" };
+        NewUser::new(&format!("User {}", i), Some(hair_color))
+    }).collect();
+    batch_insert(&data, users::table, &conn);
+
+    b.iter(|| {
+        use schema::users::dsl::*;
+        let target = users.left_outer_join(posts::table)
+            .filter(hair_color.eq("black"))
+            .order(name.desc())
+            .into_boxed();
+        target.load::<(User, Option<Post>)>(&conn).unwrap()
     })
 }
