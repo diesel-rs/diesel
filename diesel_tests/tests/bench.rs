72ed447524d736e6ca5d25a9878586465eb1b48e
Pascal Hertleif <killercup@gmail.com>
2016-04-23 19:57:05 +0200

    Execute Benchmarks for 0, 1, 10, 100, 1k, 10k Rows
    This moves benchmark code into macros to easily abstract over the number
    of rows to benchmark. Because you can't use macros in function name
    position, we have to write each benchmark name ourselves. (I tried
    moving the fns into a named mod, but there were some visibility issues
    and I gave up on that.)
    
    For the numbers in the benchmark names to align, they are padded with
    several underscores. For this to not print warning,
    `#![allow(non_snake_case)]` was added.

--- diesel_tests/tests/bench.rs
+++ diesel_tests/tests/bench.rs
@@ -1,5 +1,8 @@
 #![feature(custom_derive, plugin, custom_attribute, test)]
 #![plugin(diesel_codegen, dotenv_macros)]
+
+#![allow(non_snake_case)]
+
 #[macro_use]
 extern crate diesel;
 extern crate test;
@@ -22,90 +25,103 @@ fn connection() -> TestConnection {
     schema::connection()
 }
 
-#[bench]
-fn bench_selecting_0_rows_with_trivial_query(b: &mut Bencher) {
-    let conn = connection();
+macro_rules! bench_trivial_query {
+    ($n:expr, $name:ident, $name_boxed:ident) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            let conn = connection();
 
-    b.iter(|| {
-        users::table.load::<User>(&conn).unwrap();
-    })
+            let data: Vec<_> = (0..$n).map(|i| {
+                NewUser::new(&format!("User {}", i), None)
+            }).collect();
+            batch_insert(&data, users::table, &conn);
+
+            b.iter(|| {
+                users::table.load::<User>(&conn).unwrap()
+            })
+        }
+
+        #[bench]
+        fn $name_boxed(b: &mut Bencher) {
+            let conn = connection();
+
+            let data: Vec<_> = (0..$n).map(|i| {
+                NewUser::new(&format!("User {}", i), None)
+            }).collect();
+            batch_insert(&data, users::table, &conn);
+
+            b.iter(|| {
+                users::table.into_boxed().load::<User>(&conn).unwrap()
+            })
+        }
+    };
 }
 
-#[bench]
-fn bench_selecting_10k_rows_with_trivial_query(b: &mut Bencher) {
-    let conn = connection();
-    let data: Vec<_> = (0..10_000).map(|i| {
-        NewUser::new(&format!("User {}", i), None)
-    }).collect();
-    batch_insert(&data, users::table, &conn);
+bench_trivial_query!(0,
+    bench_trivial_query_selecting______0_rows, bench_trivial_query_selecting______0_rows_boxed);
+bench_trivial_query!(1,
+    bench_trivial_query_selecting______1_row, bench_trivial_query_selecting______1_row_boxed);
+bench_trivial_query!(10,
+    bench_trivial_query_selecting_____10_rows, bench_trivial_query_selecting_____10_rows_boxed);
+bench_trivial_query!(100,
+    bench_trivial_query_selecting____100_rows, bench_trivial_query_selecting____100_rows_boxed);
+bench_trivial_query!(1_000,
+    bench_trivial_query_selecting__1_000_rows, bench_trivial_query_selecting__1_000_rows_boxed);
+bench_trivial_query!(10_000,
+    bench_trivial_query_selecting_10_000_rows, bench_trivial_query_selecting_10_000_rows_boxed);
 
-    b.iter(|| {
-        users::table.load::<User>(&conn).unwrap()
-    })
+macro_rules! bench_medium_complex_query {
+    ($n:expr, $name:ident, $name_boxed:ident) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            let conn = connection();
+
+            let data: Vec<_> = (0..$n).map(|i| {
+                let hair_color = if i % 2 == 0 { "black" } else { "brown" };
+                NewUser::new(&format!("User {}", i), Some(hair_color))
+            }).collect();
+            batch_insert(&data, users::table, &conn);
+
+            b.iter(|| {
+                use schema::users::dsl::*;
+                let target = users.left_outer_join(posts::table)
+                    .filter(hair_color.eq("black"))
+                    .order(name.desc());
+                target.load::<(User, Option<Post>)>(&conn).unwrap()
+            })
+        }
+
+        #[bench]
+        fn $name_boxed(b: &mut Bencher) {
+            let conn = connection();
+
+            let data: Vec<_> = (0..$n).map(|i| {
+                let hair_color = if i % 2 == 0 { "black" } else { "brown" };
+                NewUser::new(&format!("User {}", i), Some(hair_color))
+            }).collect();
+            batch_insert(&data, users::table, &conn);
+
+            b.iter(|| {
+                use schema::users::dsl::*;
+                let target = users.left_outer_join(posts::table)
+                    .filter(hair_color.eq("black"))
+                    .order(name.desc())
+                    .into_boxed();
+                target.load::<(User, Option<Post>)>(&conn).unwrap()
+            })
+        }
+    };
 }
 
-#[bench]
-fn bench_selecting_10k_rows_with_trivial_query_boxed(b: &mut Bencher) {
-    let conn = connection();
-    let data: Vec<_> = (0..10_000).map(|i| {
-        NewUser::new(&format!("User {}", i), None)
-    }).collect();
-    batch_insert(&data, users::table, &conn);
-
-    b.iter(|| {
-        users::table.into_boxed().load::<User>(&conn).unwrap()
-    })
-}
-
-
-#[bench]
-fn bench_selecting_0_rows_with_medium_complex_query(b: &mut Bencher) {
-    let conn = connection();
-
-    b.iter(|| {
-        use schema::users::dsl::*;
-        let target = users.left_outer_join(posts::table)
-            .filter(hair_color.eq("black"))
-            .order(name.desc());
-        target.load::<(User, Option<Post>)>(&conn).unwrap()
-    })
-}
-
-#[bench]
-fn bench_selecting_10k_rows_with_medium_complex_query(b: &mut Bencher) {
-    let conn = connection();
-
-    let data: Vec<_> = (0..10_000).map(|i| {
-        let hair_color = if i % 2 == 0 { "black" } else { "brown" };
-        NewUser::new(&format!("User {}", i), Some(hair_color))
-    }).collect();
-    batch_insert(&data, users::table, &conn);
-
-    b.iter(|| {
-        use schema::users::dsl::*;
-        let target = users.left_outer_join(posts::table)
-            .filter(hair_color.eq("black"))
-            .order(name.desc());
-        target.load::<(User, Option<Post>)>(&conn).unwrap()
-    })
-}
-
-#[bench]
-fn bench_selecting_10k_rows_with_medium_complex_query_boxed(b: &mut Bencher) {
-    let conn = connection();
-
-    let data: Vec<_> = (0..10_000).map(|i| {
-        let hair_color = if i % 2 == 0 { "black" } else { "brown" };
-        NewUser::new(&format!("User {}", i), Some(hair_color))
-    }).collect();
-    batch_insert(&data, users::table, &conn);
-
-    b.iter(|| {
-        use schema::users::dsl::*;
-        let target = users.left_outer_join(posts::table)
-            .filter(hair_color.eq("black"))
-            .order(name.desc())
-            .into_boxed();
-        target.load::<(User, Option<Post>)>(&conn).unwrap()
-    })
-}
+bench_medium_complex_query!(0,
+    bench_medium_complex_query_selecting______0_rows, bench_medium_complex_query_selecting______0_rows_boxed);
+bench_medium_complex_query!(1,
+    bench_medium_complex_query_selecting______1_row, bench_medium_complex_query_selecting______1_row_boxed);
+bench_medium_complex_query!(10,
+    bench_medium_complex_query_selecting_____10_rows, bench_medium_complex_query_selecting_____10_rows_boxed);
+bench_medium_complex_query!(100,
+    bench_medium_complex_query_selecting____100_rows, bench_medium_complex_query_selecting____100_rows_boxed);
+bench_medium_complex_query!(1_000,
+    bench_medium_complex_query_selecting__1_000_rows, bench_medium_complex_query_selecting__1_000_rows_boxed);
+bench_medium_complex_query!(10_000,
+    bench_medium_complex_query_selecting_10_000_rows, bench_medium_complex_query_selecting_10_000_rows_boxed);
