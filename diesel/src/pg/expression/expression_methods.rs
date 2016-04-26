e0ac4ed545d2678aaed97bdb272d26ab779c12af
Sean Griffin <sean@seantheprogrammer.com>
2016-04-26 16:35:16 -0600

    Add support for comparison operators when used with PG arrays
    These were all fairly straightforward. We used methods for all three,
    even though one maps to the `&&` operator in PG. This is because we
    can't overload the return type of `&&` in Rust, and we probably wouldn't
    do that even if we could because I think `&&` is an abysmally unclear
    operator for "overlap"
    
    We had to add a dummy `users` table because of the call to `Insertable!`
    in the doctest setup file. We can't just do it there, because a few
    examples need a different schema. I'm not sure how I want to restructure
    it yet, but those will be removed in a future commit.

--- diesel/src/pg/expression/expression_methods.rs
+++ diesel/src/pg/expression/expression_methods.rs
@@ -1,5 +1,6 @@
 use expression::{Expression, AsExpression};
 use super::predicates::*;
+use types::Array;
 
 pub trait PgExpressionMethods: Expression + Sized {
     /// Creates a PostgreSQL `IS NOT DISTINCT FROM` expression. This behaves
@@ -49,3 +50,164 @@ pub trait PgTimestampExpressionMethods: Expression<SqlType=Timestamp> + Sized {
 }
 
 impl<T: Expression<SqlType=Timestamp>> PgTimestampExpressionMethods for T {}
+
+pub trait ArrayExpressionMethods<ST>: Expression<SqlType=Array<ST>> + Sized {
+    /// Compares two arrays for common elements, using the `&&` operator in
+    /// the final SQL
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # #[macro_use] extern crate diesel;
+    /// # include!("src/doctest_setup.rs");
+    /// #
+    /// # table! {
+    /// #     posts {
+    /// #         id -> Integer,
+    /// #         tags -> Array<VarChar>,
+    /// #     }
+    /// # }
+    /// #
+    /// # // FIXME: We shouldn't need to define a users table here
+    /// # table! {
+    /// #     users {
+    /// #         id -> Integer,
+    /// #         name -> VarChar,
+    /// #     }
+    /// # }
+    /// #
+    /// # struct NewPost<'a> { tags: Vec<&'a str> }
+    /// # Insertable! { (posts) struct NewPost<'a> { tags: Vec<&'a str>, } }
+    /// #
+    /// # fn main() {
+    /// #     use self::posts::dsl::*;
+    /// #     let conn = establish_connection();
+    /// #     conn.execute("DROP TABLE IF EXISTS posts").unwrap();
+    /// #     conn.execute("CREATE TABLE posts (id SERIAL PRIMARY KEY, tags TEXT[] NOT NULL)").unwrap();
+    /// #
+    /// diesel::insert(&vec![
+    ///     NewPost { tags: vec!["cool", "awesome"] },
+    ///     NewPost { tags: vec!["awesome", "great"] },
+    ///     NewPost { tags: vec!["cool", "great"] },
+    /// ]).into(posts).execute(&conn).unwrap();
+    ///
+    /// let query = posts.select(id).filter(tags.overlaps_with(vec!["horrid", "cool"]));
+    /// assert_eq!(Ok(vec![1, 3]), query.load(&conn));
+    ///
+    /// let query = posts.select(id).filter(tags.overlaps_with(vec!["cool", "great"]));
+    /// assert_eq!(Ok(vec![1, 2, 3]), query.load(&conn));
+    ///
+    /// let query = posts.select(id).filter(tags.overlaps_with(vec!["horrid"]));
+    /// assert_eq!(Ok(Vec::new()), query.load::<i32>(&conn));
+    /// # }
+    /// ```
+    fn overlaps_with<T>(self, other: T) -> OverlapsWith<Self, T::Expression> where
+        T: AsExpression<Self::SqlType>,
+    {
+        OverlapsWith::new(self, other.as_expression())
+    }
+
+    /// Compares whether an array contains another array, using the `@>` operator.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # #[macro_use] extern crate diesel;
+    /// # include!("src/doctest_setup.rs");
+    /// #
+    /// # table! {
+    /// #     posts {
+    /// #         id -> Integer,
+    /// #         tags -> Array<VarChar>,
+    /// #     }
+    /// # }
+    /// #
+    /// # // FIXME: We shouldn't need to define a users table here
+    /// # table! {
+    /// #     users {
+    /// #         id -> Integer,
+    /// #         name -> VarChar,
+    /// #     }
+    /// # }
+    /// #
+    /// # struct NewPost<'a> { tags: Vec<&'a str> }
+    /// # Insertable! { (posts) struct NewPost<'a> { tags: Vec<&'a str>, } }
+    /// #
+    /// # fn main() {
+    /// #     use self::posts::dsl::*;
+    /// #     let conn = establish_connection();
+    /// #     conn.execute("DROP TABLE IF EXISTS posts").unwrap();
+    /// #     conn.execute("CREATE TABLE posts (id SERIAL PRIMARY KEY, tags TEXT[] NOT NULL)").unwrap();
+    /// #
+    /// diesel::insert(&vec![
+    ///     NewPost { tags: vec!["cool", "awesome"] },
+    /// ]).into(posts).execute(&conn).unwrap();
+    ///
+    /// let query = posts.select(id).filter(tags.contains(vec!["cool"]));
+    /// assert_eq!(Ok(vec![1]), query.load(&conn));
+    ///
+    /// let query = posts.select(id).filter(tags.contains(vec!["cool", "amazing"]));
+    /// assert_eq!(Ok(Vec::new()), query.load::<i32>(&conn));
+    /// # }
+    /// ```
+    fn contains<T>(self, other: T) -> Contains<Self, T::Expression> where
+        T: AsExpression<Self::SqlType>,
+    {
+        Contains::new(self, other.as_expression())
+    }
+
+    /// Compares whether an array is contained by another array, using the `<@` operator.
+    /// This is the opposite of `contains`
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # #[macro_use] extern crate diesel;
+    /// # include!("src/doctest_setup.rs");
+    /// #
+    /// # table! {
+    /// #     posts {
+    /// #         id -> Integer,
+    /// #         tags -> Array<VarChar>,
+    /// #     }
+    /// # }
+    /// #
+    /// # // FIXME: We shouldn't need to define a users table here
+    /// # table! {
+    /// #     users {
+    /// #         id -> Integer,
+    /// #         name -> VarChar,
+    /// #     }
+    /// # }
+    /// #
+    /// # struct NewPost<'a> { tags: Vec<&'a str> }
+    /// # Insertable! { (posts) struct NewPost<'a> { tags: Vec<&'a str>, } }
+    /// #
+    /// # fn main() {
+    /// #     use self::posts::dsl::*;
+    /// #     let conn = establish_connection();
+    /// #     conn.execute("DROP TABLE IF EXISTS posts").unwrap();
+    /// #     conn.execute("CREATE TABLE posts (id SERIAL PRIMARY KEY, tags TEXT[] NOT NULL)").unwrap();
+    /// #
+    /// diesel::insert(&vec![
+    ///     NewPost { tags: vec!["cool", "awesome"] },
+    /// ]).into(posts).execute(&conn).unwrap();
+    ///
+    /// let query = posts.select(id).filter(tags.is_contained_by(vec!["cool", "awesome", "amazing"]));
+    /// assert_eq!(Ok(vec![1]), query.load(&conn));
+    ///
+    /// let query = posts.select(id).filter(tags.is_contained_by(vec!["cool"]));
+    /// assert_eq!(Ok(Vec::new()), query.load::<i32>(&conn));
+    /// # }
+    /// ```
+    fn is_contained_by<T>(self, other: T) -> IsContainedBy<Self, T::Expression> where
+        T: AsExpression<Self::SqlType>,
+    {
+        IsContainedBy::new(self, other.as_expression())
+    }
+}
+
+impl<T, ST> ArrayExpressionMethods<ST> for T where
+    T: Expression<SqlType=Array<ST>>,
+{
+}
--- diesel/src/pg/expression/predicates.rs
+++ diesel/src/pg/expression/predicates.rs
@@ -1,3 +1,6 @@
 use pg::Pg;
 
 infix_predicate!(IsNotDistinctFrom, " IS NOT DISTINCT FROM ", backend: Pg);
+infix_predicate!(OverlapsWith, " && ", backend: Pg);
+infix_predicate!(Contains, " @> ", backend: Pg);
+infix_predicate!(IsContainedBy, " <@ ", backend: Pg);
