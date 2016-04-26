a78dc4d0b61311b3de5bee38d1a17f7a55c0f707
Sean Griffin <sean@seantheprogrammer.com>
2016-04-26 07:45:42 -0600

    Add the same debug check we do for integers to floats
    This has the same reasoning. If you're using the `sql` function (meaning
    you're manually marking the types), it's easy to mistake a function
    which returns a double for one which returns a float. This assertion is
    in debug mode only, as this problem does not affect our query builder
    which enforces correctness in types.

--- diesel/src/types/impls/floats.rs
+++ diesel/src/types/impls/floats.rs
@@ -10,6 +10,8 @@ use types::{self, FromSql, ToSql, IsNull};
 impl<DB: Backend<RawValue=[u8]>> FromSql<types::Float, DB> for f32 {
     fn from_sql(bytes: Option<&[u8]>) -> Result<Self, Box<Error+Send+Sync>> {
         let mut bytes = not_none!(bytes);
+        debug_assert!(bytes.len() <= 4, "Received more than 4 bytes while decoding \
+                      an f32. Was a double accidentally marked as float?");
         bytes.read_f32::<BigEndian>().map_err(|e| Box::new(e) as Box<Error+Send+Sync>)
     }
 }
@@ -25,6 +27,8 @@ impl<DB: Backend> ToSql<types::Float, DB> for f32 {
 impl<DB: Backend<RawValue=[u8]>> FromSql<types::Double, DB> for f64 {
     fn from_sql(bytes: Option<&[u8]>) -> Result<Self, Box<Error+Send+Sync>> {
         let mut bytes = not_none!(bytes);
+        debug_assert!(bytes.len() <= 8, "Received more than 8 bytes while decoding \
+                      an f64. Was a numeric accidentally marked as dobule?");
         bytes.read_f64::<BigEndian>().map_err(|e| Box::new(e) as Box<Error+Send+Sync>)
     }
 }
