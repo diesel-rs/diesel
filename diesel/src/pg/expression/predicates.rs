9d4054c62db3b3d92c24fc6e58d40b3011f74ba4
Rasmus Kaj <kaj@kth.se>
2016-08-07 00:43:17 +0200

    Add support for ordering with NULLS FIRST or NULLS LAST in postgres.

--- diesel/src/pg/expression/predicates.rs
+++ diesel/src/pg/expression/predicates.rs
@@ -1,6 +1,9 @@
 use pg::Pg;
+use query_builder::QueryBuilder;
 
 infix_predicate!(IsNotDistinctFrom, " IS NOT DISTINCT FROM ", backend: Pg);
 infix_predicate!(OverlapsWith, " && ", backend: Pg);
 infix_predicate!(Contains, " @> ", backend: Pg);
 infix_predicate!(IsContainedBy, " <@ ", backend: Pg);
+postfix_expression!(NullsFirst, " NULLS FIRST", ());
+postfix_expression!(NullsLast, " NULLS LAST", ());
