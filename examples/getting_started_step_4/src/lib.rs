bcfbd4238d37b63b28a49140efe57d6a908744d4
Sean Griffin <sean@seantheprogrammer.com>
2016-08-15 12:36:03 -0400

    Add getting started example step 4

---
+++ examples/getting_started_step_4/src/lib.rs
@@ -0,0 +1,40 @@
+#![cfg_attr(feature = "nightly", feature(custom_derive, custom_attribute, plugin))]
+#![cfg_attr(feature = "nightly", plugin(diesel_codegen, dotenv_macros))]
+
+#[macro_use] extern crate diesel;
+extern crate dotenv;
+
+#[cfg(feature = "nightly")]
+include!("lib.in.rs");
+
+#[cfg(feature = "with-syntex")]
+include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+use self::models::{Post, NewPost};
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &PgConnection, title: &str, body: &str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert(&new_post).into(posts::table)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
