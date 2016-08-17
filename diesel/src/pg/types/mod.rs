306ad26711a53bf151153377a87525d943b3c60e
Sean Griffin <sean@seantheprogrammer.com>
2016-08-17 09:52:33 -0400

    Better document the `types` module
    I've seen a lot of people making the mistake of trying to put things
    like `diesel::types::Timestamp` in their structs. This attempts to
    better document the role of the `types` module, and documents any
    `ToSql` and `FromSql` impls that are provided. I've un-inlined the pg
    types module, as stating "this is a postgres specific type" in the
    documentation over and over again didn't feel right. I've also made sure
    that the UUID feature is turned on when generating docs.

--- diesel/src/pg/types/mod.rs
+++ diesel/src/pg/types/mod.rs
@@ -6,16 +6,66 @@ mod primitives;
 #[cfg(feature = "uuid")]
 mod uuid;
 
-#[doc(hidden)]
+/// PostgreSQL specific SQL types
+///
+/// Note: All types in this module can be accessed through `diesel::types`
 pub mod sql_types {
+    /// The OID SQL type. This is a PostgreSQL specific type.
+    ///
+    /// ### [`ToSql`](/diesel/types/trait.ToSql.html) impls
+    ///
+    /// - [`u32`][u32]
+    ///
+    /// ### [`FromSql`](/diesel/types/trait.FromSql.html) impls
+    ///
+    /// - [`u32`][u32]
+    ///
+    /// [u32]: https://doc.rust-lang.org/nightly/std/primitive.u32.html
     #[derive(Debug, Clone, Copy, Default)] pub struct Oid;
-    #[derive(Debug, Clone, Copy, Default)] pub struct Array<T>(T);
+
+    /// The Array SQL type. This wraps another type to represent a SQL array of
+    /// that type. Multidimensional arrays are not supported, nor are arrays
+    /// containing null.
+    ///
+    /// ### [`ToSql`](/diesel/types/trait.ToSql.html) impls
+    ///
+    /// - [`Vec<T>`][Vec] for any `T` which implements `ToSql<ST>`
+    /// - [`&[T]`][slice] for any `T` which implements `ToSql<ST>`
+    ///
+    /// ### [`FromSql`](/diesel/types/trait.FromSql.html) impls
+    ///
+    /// - [`Vec<T>`][Vec] for any `T` which implements `ToSql<ST>`
+    ///
+    /// [Vec]: https://doc.rust-lang.org/nightly/std/vec/struct.Vec.html
+    /// [slice]: https://doc.rust-lang.org/nightly/std/primitive.slice.html
+    #[derive(Debug, Clone, Copy, Default)] pub struct Array<ST>(ST);
+
+    /// Alias for SmallInt
     pub type SmallSerial = ::types::SmallInt;
+
+    /// Alias for Integer
     pub type Serial = ::types::Integer;
+
+    /// Alias for BigInt
     pub type BigSerial = ::types::BigInt;
+
     #[cfg(feature = "uuid")]
+    /// The UUID SQL type. This type can only be used with `feature = "uuid"`
+    ///
+    /// ### [`ToSql`](/diesel/types/trait.ToSql.html) impls
+    ///
+    /// - [`uuid::Uuid`][Uuid]
+    ///
+    /// ### [`FromSql`](/diesel/types/trait.FromSql.html) impls
+    ///
+    /// - [`uuid::Uuid`][Uuid]
+    ///
+    /// [Vec]: https://doc.rust-lang.org/uuid/uuid/struct.Uuid.html
     #[derive(Debug, Clone, Copy, Default)] pub struct Uuid;
+
+    /// Alias for `Binary`, to ensure `infer_schema!` works
     pub type Bytea = ::types::Binary;
+
     #[doc(hidden)]
     pub type Bpchar = ::types::VarChar;
 }
