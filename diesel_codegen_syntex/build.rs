f2e74ef8dfa8f298f9090f31cd39185bba94ae5c
Sean Griffin <sean@seantheprogrammer.com>
2016-08-20 09:07:10 -0400

    Increase stack size when building with syntex
    I've been getting stack overflow errors when building. Serde 0.42.0
    includes a helper for this. I don't want to bump our minimum required
    version though, so I've included a pared down version of the function.

--- diesel_codegen_syntex/build.rs
+++ diesel_codegen_syntex/build.rs
@@ -5,38 +5,46 @@ mod inner {
 
     use std::env;
     use std::path::Path;
+    use std::thread;
 
     use self::syntax::codemap::Span;
     use self::syntax::ext::base::{self, ExtCtxt};
     use self::syntax::tokenstream::TokenTree;
 
     pub fn main() {
-        let out_dir = env::var_os("OUT_DIR").unwrap();
-        let mut registry = syntex::Registry::new();
+        with_extra_stack(move || {
+            let out_dir = env::var_os("OUT_DIR").unwrap();
+            let mut registry = syntex::Registry::new();
 
-        macro_rules! register_quote_macro {
-            ($macro_name: ident, $name: ident) => {
-                fn $name<'cx>(
-                    cx: &'cx mut ExtCtxt,
-                    sp: Span,
-                    tts: &[TokenTree],
-                ) -> Box<base::MacResult + 'cx> {
-                    syntax::ext::quote::$name(cx, sp, tts)
+            macro_rules! register_quote_macro {
+                ($macro_name: ident, $name: ident) => {
+                    fn $name<'cx>(
+                        cx: &'cx mut ExtCtxt,
+                        sp: Span,
+                        tts: &[TokenTree],
+                    ) -> Box<base::MacResult + 'cx> {
+                        syntax::ext::quote::$name(cx, sp, tts)
+                    }
+
+                    registry.add_macro(stringify!($macro_name), $name);
                 }
-
-                registry.add_macro(stringify!($macro_name), $name);
             }
-        }
 
-        register_quote_macro!(quote_ty, expand_quote_ty);
-        register_quote_macro!(quote_item, expand_quote_item);
-        register_quote_macro!(quote_tokens, expand_quote_tokens);
-        register_quote_macro!(quote_expr, expand_quote_expr);
+            register_quote_macro!(quote_ty, expand_quote_ty);
+            register_quote_macro!(quote_item, expand_quote_item);
+            register_quote_macro!(quote_tokens, expand_quote_tokens);
+            register_quote_macro!(quote_expr, expand_quote_expr);
 
-        let src = Path::new("src/lib.in.rs");
-        let dst = Path::new(&out_dir).join("lib.rs");
+            let src = Path::new("src/lib.in.rs");
+            let dst = Path::new(&out_dir).join("lib.rs");
 
-        registry.expand("", &src, &dst).unwrap();
+            registry.expand("", &src, &dst).unwrap();
+        });
+    }
+
+    fn with_extra_stack<F: FnOnce() + Send + 'static>(f: F) {
+        env::set_var("RUST_MIN_STACK", "16777216"); // 16MB
+        thread::spawn(f).join().unwrap();
     }
 }
 
