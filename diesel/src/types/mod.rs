4b94471c584a406c8787671a0f4ceed48842e88c
Georg Semmler <georg_semmler_05@web.de>
2016-09-06 23:37:04 +0200

    Simplify the support of custom types

--- diesel/src/types/mod.rs
+++ diesel/src/types/mod.rs
@@ -5,6 +5,8 @@ mod ord;
 #[macro_use]
 #[doc(hidden)]
 pub mod impls;
+#[macro_use]
+pub mod custom_type;
 mod fold;
 
 #[doc(hidden)]
@@ -25,6 +27,8 @@ pub use self::ord::SqlOrd;
 
 /// Marker trait for types which can be folded for a sum.
 pub use self::fold::Foldable;
+
+pub use self::custom_type::CustomSqlType;
 
 use backend::{Backend, TypeMetadata};
 use row::Row;
