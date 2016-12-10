67bf22deb3d110b60bb6b56d36683a1fdefb19ff
Sean Griffin <sean@seantheprogrammer.com>
2016-12-10 06:17:14 -0500

    Document `diesel::row::Row`
    I had excluded this trait, as I intended for it to be mostly internal. I
    still think this should be the case, but until I can provide a proper
    blanket impl for `FromSqlRow`, this type will need to be public.
    
    Fixes #535.

--- diesel/src/row.rs
+++ diesel/src/row.rs
@@ -1,5 +1,9 @@
 use backend::Backend;
 
+/// The row trait which is used for [`FromSqlRow`][]. Apps should not need to
+/// concern themselves with this trait.
+///
+/// [`FromSqlRow`]: ../types/trait.FromSqlRow.html
 pub trait Row<DB: Backend> {
     fn take(&mut self) -> Option<&DB::RawValue>;
     fn next_is_null(&self, count: usize) -> bool;
