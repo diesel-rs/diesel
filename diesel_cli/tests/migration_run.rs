85d0ecfe40e7e88507f5fffbc01e82ba00caa2ac
Adrian Perez de Castro <aperez@igalia.com>
2017-01-02 18:46:39 +0200

    Test that running migrations adds valid timestamps
    This adds a test case which checks that the __diesel_schema_migrations table
    is initially empty, and that after running a migration, the table is non-empty
    and the timestamp in the "run_on" has a valid value which can be compared to
    an older timestamp.

--- diesel_cli/tests/migration_run.rs
+++ diesel_cli/tests/migration_run.rs
@@ -1,4 +1,7 @@
 use support::{database, project};
+use diesel::{select, LoadDsl};
+use diesel::expression::sql;
+use diesel::types::Bool;
 
 #[test]
 fn migration_run_runs_pending_migrations() {
@@ -24,4 +27,56 @@ fn migration_run_runs_pending_migrations() {
     assert!(result.stdout().contains("Running migration 12345"),
         "Unexpected stdout {}", result.stdout());
     assert!(db.table_exists("users"));
+}
+
+#[test]
+fn migration_run_inserts_run_on_timestamps() {
+    let p = project("migration_run_on_timestamps")
+        .folder("migrations")
+        .build();
+    let db = database(&p.database_url());
+
+    // Make sure the project is setup.
+    p.command("setup").run();
+
+    p.create_migration("12345_create_users_table",
+                       "CREATE TABLE users ( id INTEGER )",
+                       "DROP TABLE users");
+
+    let migrations_done: bool = select(sql::<Bool>(
+            "EXISTS (SELECT * FROM __diesel_schema_migrations)"))
+        .get_result(&db.conn())
+        .unwrap();
+    assert!(!migrations_done, "Migrations table should be empty");
+
+    let result = p.command("migration")
+        .arg("run")
+        .run();
+
+    assert!(result.is_success(), "Result was unsuccessful {:?}", result);
+    assert!(db.table_exists("users"));
+
+    // By running a query that compares timestamps, we are also checking
+    // that the auto-inserted values for the "run_on" column are valid.
+
+    #[cfg(feature = "sqlite")]
+    fn valid_run_on_timestamp(db: &database::Database) -> bool {
+        select(sql::<Bool>("EXISTS (SELECT run_on < DATETIME('now', '-1 hour') \
+                                    FROM __diesel_schema_migrations)"))
+            .get_result(&db.conn())
+            .unwrap()
+    }
+
+    #[cfg(feature = "postgres")]
+    fn valid_run_on_timestamp(db: &database::Database) -> bool {
+        select(sql::<Bool>("EXISTS (SELECT \
+                              run_on < (CAST('now' AS TIMESTAMP) - \
+                                        CAST('1 hour' AS INTERVAL)) \
+                              FROM __diesel_schema_migrations)"))
+            .get_result(&db.conn())
+            .unwrap()
+    }
+
+    assert!(valid_run_on_timestamp(&db),
+            "Running a migration did not insert an updated run_on value");
 }
