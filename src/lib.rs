9295916338e77430eb2a08752f38320f8ba4f95b
Taryn Hill <taryn@phrohdoh.com>
2017-01-23 20:17:33 -0600

    Getting started step 2

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,26 @@ use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use std::env;
 
+use models::NewPost;
+
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_post<'a>(conn: &SqliteConnection, title: &'a str, body: &'a str) -> usize {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert(&new_post)
+        .into(posts::table)
+        .execute(conn)
+        .expect("Error saving new post")
 }
