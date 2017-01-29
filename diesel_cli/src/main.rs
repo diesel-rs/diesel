6c540c08b5f2c5a9e79f2054cb069a4c60c00949
Georg Semmler <georg_semmler_05@web.de>
2017-01-29 21:51:57 +0100

    Simplify print-schema cli command
    * only load those information about those tables from the database that
    are really requested

--- diesel_cli/src/main.rs
+++ diesel_cli/src/main.rs
@@ -210,21 +210,18 @@ fn run_infer_schema(matches: &ArgMatches) {
         ));
 
     let tables = table_names.iter()
-        .map(|table| {
-            diesel_infer_schema::infer_schema_for_schema_name(table, &database_url)
-                .expect(&format!("Could not load table `{}`", table.to_string()))
-        })
-        .filter(|table| {
+        .filter_map(|table| {
             let table_name = table.to_string();
             if is_whitelist && !filtering_tables.contains(&table_name[..]) {
-                return false;
+                return None;
             }
             if is_blacklist && filtering_tables.contains(&table_name[..]) {
-                return false;
+                return None;
             }
-            true
-        })
-        .map(|t| t.tokens());
+            Some(diesel_infer_schema::infer_schema_for_schema_name(table, &database_url)
+                .expect(&format!("Could not load table `{}`", table.to_string()))
+                .tokens())
+        });
 
     let schema = diesel_infer_schema::handle_schema(tables, schema_name);
 
