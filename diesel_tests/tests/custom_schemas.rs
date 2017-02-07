e0272a4398444666ea7a62b2cf1e6a98003fd17e
Sean Griffin <sean@seantheprogrammer.com>
2017-02-07 11:47:59 -0500

    Properly load tables from `infer_table_from_schema!`
    This bug was introduced by #577, which completely ignores any table
    names passed to it which specify a custom schema. This bug slipped
    through because that same PR also changed the implementation of
    `infer_schema!` to not call `infer_table_from_schema!`. We didn't have
    an explicit test for both macros in this case, since we usually
    structure our code to reuse itself as much as possible, and fully
    exercise as many code paths as we can so we don't have to consider every
    combination in every place all the time.
    
    This reverts to the original behavior of that macro, and adds a more
    explicit test so it does not happen again.

--- diesel_tests/tests/custom_schemas.rs
+++ diesel_tests/tests/custom_schemas.rs
@@ -1,22 +1,50 @@
 use diesel::*;
 use schema::connection;
 
-infer_schema!("dotenv:DATABASE_URL", "custom_schema");
-use self::custom_schema::users;
+mod using_infer_schema {
+    use super::*;
+    infer_schema!("dotenv:DATABASE_URL", "custom_schema");
+    use self::custom_schema::users;
 
-#[derive(Insertable)]
-#[table_name="users"]
-struct NewUser {
-    id: i32,
+    #[derive(Insertable)]
+    #[table_name="users"]
+    struct NewUser {
+        id: i32,
+    }
+
+    #[test]
+    fn custom_schemas_are_loaded_by_infer_schema() {
+        let conn = connection();
+        insert(&NewUser { id: 1 }).into(users::table)
+            .execute(&conn).unwrap();
+        let users = users::table.select(users::id)
+            .load(&conn);
+
+        assert_eq!(Ok(vec![1]), users);
+    }
 }
 
-#[test]
-fn custom_schemas_are_loaded_by_infer_schema() {
-    let conn = connection();
-    insert(&NewUser { id: 1 }).into(users::table)
-        .execute(&conn).unwrap();
-    let users = users::table.select(users::id)
-        .load(&conn);
+mod using_infer_table_from_schema {
+    use super::*;
+    mod infer_users {
+        infer_table_from_schema!("dotenv:DATABASE_URL", "custom_schema.users");
+    }
+    use self::infer_users::users;
 
-    assert_eq!(Ok(vec![1]), users);
+    #[derive(Insertable)]
+    #[table_name="users"]
+    struct NewUser {
+        id: i32,
+    }
+
+    #[test]
+    fn custom_schemas_are_loaded_by_infer_table_from_schema() {
+        let conn = connection();
+        insert(&NewUser { id: 1 }).into(users::table)
+            .execute(&conn).unwrap();
+        let users = users::table.select(users::id)
+            .load(&conn);
+
+        assert_eq!(Ok(vec![1]), users);
+    }
 }
