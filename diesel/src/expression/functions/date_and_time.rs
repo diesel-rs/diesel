a6427efd19299266b18e7ceb60d709d21bec2593
Sean Griffin <sean@seantheprogrammer.com>
2017-02-12 13:56:03 -0500

    minor doc fixes

--- diesel/src/expression/functions/date_and_time.rs
+++ diesel/src/expression/functions/date_and_time.rs
@@ -40,5 +40,5 @@ impl_query_id!(now);
 operator_allowed!(now, Add, add);
 operator_allowed!(now, Sub, sub);
 sql_function!(date, date_t, (x: Timestamp) -> Date,
-"Represents the SQL DATE() function. The argument should be a Timestamp
+"Represents the SQL `DATE` function. The argument should be a Timestamp
 expression, and the return value will be an expression of type Date");
