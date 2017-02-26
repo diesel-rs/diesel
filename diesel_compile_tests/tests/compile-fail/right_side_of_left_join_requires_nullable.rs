dc21d3fbc192c6c95c8cd1a7a72365368196a33e
Sean Griffin <sean@seantheprogrammer.com>
2017-02-26 12:13:32 -0500

    Add some more tests around the current state of joins

---
+++ diesel_compile_tests/tests/compile-fail/right_side_of_left_join_requires_nullable.rs
@@ -0,0 +1,40 @@
+#[macro_use] extern crate diesel;
+
+use diesel::*;
+use diesel::pg::PgConnection;
+use diesel::types::Text;
+
+table! {
+    users {
+        id -> Integer,
+        name -> VarChar,
+    }
+}
+
+table! {
+    posts {
+        id -> Integer,
+        title -> Text,
+        user_id -> Integer,
+    }
+}
+
+joinable!(posts -> users (user_id));
+select_column_workaround!(posts -> users (id, title, user_id));
+select_column_workaround!(users -> posts (id, name));
+sql_function!(lower, lower_t, (x: Text) -> Text);
+
+fn main() {
+    let conn = PgConnection::establish("some url").unwrap();
+    let join = users::table.left_outer_join(posts::table);
+
+    // Valid
+    let _ = join.select(posts::title.nullable());
+    // Valid -- NULL to a function will return null
+    let _ = join.select(lower(posts::title).nullable());
+    // FIXME: This compiles and is unsound
+    // Invalid, only Nullable<title> is selectable
+    let _ = join.select(lower(posts::title));
+    // Invalid, Nullable<title> is selectable, but lower expects not-null
+    let _ = join.select(lower(posts::title.nullable())); //~ ERROR E0271
+}
