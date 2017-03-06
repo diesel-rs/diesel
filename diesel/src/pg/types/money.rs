1daf2581919d82b80c18f00957e5c3d35375c4c0
pfernie <patrick.fernie@gmail.com>
2017-03-06 11:52:34 -0500

    add Add/Sub impls for PgMoney (#778)
    - Avoid surprising wrapping behavior for `Add` & `Sub` by using checked_{add,sub} instead.
    - Instead of returning `Option`, `panic!` in `Add` and `Sub` impls (documented via `# Panics` sections)

--- diesel/src/pg/types/money.rs
+++ diesel/src/pg/types/money.rs
@@ -1,5 +1,6 @@
 //! Support for Money values under PostgreSQL.
 use std::error::Error;
+use std::ops::{Add, AddAssign, Sub, SubAssign};
 use std::io::prelude::*;
 
 use byteorder::{ReadBytesExt, WriteBytesExt, NetworkEndian};
@@ -39,6 +40,44 @@ impl ToSql<types::Money, Pg> for PgMoney {
     }
 }
 
+impl Add for PgMoney {
+    type Output = Self;
+    /// # Panics
+    ///
+    /// Performs a checked addition, and will `panic!` on overflow in both `debug` and `release`.
+    fn add(self, rhs: PgMoney) -> Self::Output {
+        self.0.checked_add(rhs.0).map(PgMoney).expect("overflow adding money amounts")
+    }
+}
+
+impl AddAssign for PgMoney {
+    /// # Panics
+    ///
+    /// Performs a checked addition, and will `panic!` on overflow in both `debug` and `release`.
+    fn add_assign(&mut self, rhs: PgMoney) {
+        self.0 = self.0.checked_add(rhs.0).expect("overflow adding money amounts")
+    }
+}
+
+impl Sub for PgMoney {
+    type Output = Self;
+    /// # Panics
+    ///
+    /// Performs a checked subtraction, and will `panic!` on underflow in both `debug` and `release`.
+    fn sub(self, rhs: PgMoney) -> Self::Output {
+        self.0.checked_sub(rhs.0).map(PgMoney).expect("underflow subtracting money amounts")
+    }
+}
+
+impl SubAssign for PgMoney {
+    /// # Panics
+    ///
+    /// Performs a checked subtraction, and will `panic!` on underflow in both `debug` and `release`.
+    fn sub_assign(&mut self, rhs: PgMoney) {
+        self.0 = self.0.checked_sub(rhs.0).expect("underflow subtracting money amounts")
+    }
+}
+
 #[cfg(feature = "quickcheck")]
 mod quickcheck_impls {
     extern crate quickcheck;
@@ -51,4 +90,62 @@ mod quickcheck_impls {
             PgMoney(i64::arbitrary(g))
         }
     }
+}
+
+#[test]
+fn add_money() {
+    let c1 = PgMoney(123);
+    let c2 = PgMoney(456);
+    assert_eq!(PgMoney(579), c1 + c2);
+}
+
+#[test]
+fn add_assign_money() {
+    let mut c1 = PgMoney(123);
+    c1 += PgMoney(456);
+    assert_eq!(PgMoney(579), c1);
+}
+
+#[test]
+#[should_panic(expected = "overflow adding money amounts")]
+fn add_money_overflow() {
+    let c1 = PgMoney(::std::i64::MAX);
+    let c2 = PgMoney(1);
+    let _overflow = c1 + c2;
+}
+
+#[test]
+#[should_panic(expected = "overflow adding money amounts")]
+fn add_assign_money_overflow() {
+    let mut c1 = PgMoney(::std::i64::MAX);
+    c1 += PgMoney(1);
+}
+
+#[test]
+fn sub_money() {
+    let c1 = PgMoney(123);
+    let c2 = PgMoney(456);
+    assert_eq!(PgMoney(-333), c1 - c2);
+}
+
+#[test]
+fn sub_assign_money() {
+    let mut c1 = PgMoney(123);
+    c1 -= PgMoney(456);
+    assert_eq!(PgMoney(-333), c1);
+}
+
+#[test]
+#[should_panic(expected = "underflow subtracting money amounts")]
+fn sub_money_underflow() {
+    let c1 = PgMoney(::std::i64::MIN);
+    let c2 = PgMoney(1);
+    let _underflow = c1 - c2;
+}
+
+#[test]
+#[should_panic(expected = "underflow subtracting money amounts")]
+fn sub_assign_money_underflow() {
+    let mut c1 = PgMoney(::std::i64::MIN);
+    c1 -= PgMoney(1);
 }
