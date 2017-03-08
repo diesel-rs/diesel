430362f09d551f00210f64c6158a6421d486b882
Erich Cordoba <erich.cm@yandex.com>
2017-03-07 21:31:00 -0600

    Split sqlite getting started example

---
+++ examples/sqlite/getting_started_step_2/src/lib.rs
@@ -0,0 +1,37 @@
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
+extern crate dotenv;
+
+pub mod schema;
+pub mod models;
+
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
+use std::env;
+
+use self::models::NewPost;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+    .expect(&format!("Error connecting to {}",
+    database_url))
+}
+
+pub fn create_post(conn: &SqliteConnection, title: &str, body: &str) -> usize {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert(&new_post)
+        .into(posts::table)
+        .execute(conn)
+        .expect("Error saving new post")
+}
