8c8c0ab9d1c877650ffcb804ac1ba6d96ed591b9
Erich Cordoba <erich.cm@yandex.com>
2017-03-09 12:10:37 -0600

    Split sqlite getting started example (#780)
    Fix #776

---
+++ examples/sqlite/getting_started_step_1/src/lib.rs
@@ -0,0 +1,21 @@
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
+extern crate dotenv;
+
+pub mod schema;
+pub mod models;
+
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+    .expect(&format!("Error connecting to {}",
+    database_url))
+}
---
+++ examples/sqlite/getting_started_step_2/src/bin/write_post.rs
@@ -0,0 +1,28 @@
+extern crate diesel_demo_step_2_sqlite;
+extern crate diesel;
+
+use self::diesel_demo_step_2_sqlite::*;
+use std::io::{stdin, Read};
+
+fn main() {
+    let connection = establish_connection();
+
+    println!("What would you like your title to be?");
+    let mut title = String::new();
+    stdin().read_line(&mut title).unwrap();
+    let title = &title[..(title.len() - 1)]; // Drop the newline character
+    println!("\nOk! Let's write {} (Press {} when finished)\n",
+             title,
+             EOF);
+    let mut body = String::new();
+    stdin().read_to_string(&mut body).unwrap();
+
+    let _ = create_post(&connection, title, &body);
+    println!("\nSaved draft {}", title);
+}
+
+#[cfg(not(windows))]
+const EOF: &'static str = "CTRL+D";
+
+#[cfg(windows)]
+const EOF: &'static str = "CTRL+Z";
