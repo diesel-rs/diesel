9c244ee1d2cd353c10f466f8dc3ed4b602a80da1
Dorian Scheidt <dorian.scheidt@gmail.com>
2017-06-03 17:36:40 -0500

    Add error message for derive(Insertable) on empty struct
    This change adds a new error message when attempting to derive
    Insertable on a struct with no fields, and a compile-fail test to
    ensure it is displayed appropriately.
    
    See #920

---
+++ diesel_compile_tests/tests/compile-fail/empty_insertable_struct.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
+
+use diesel::*;
+
+table! {
+    users {
+        id -> Integer,
+    }
+}
+
+#[derive(Insertable)]
+//~^ ERROR proc-macro derive panicked
+//~| HELP Failed to derive `Insertable` for `NewUser`: `Insertable` cannot be used on structs with empty fields
+#[table_name="users"]
+pub struct NewUser {}
+
+fn main() {
+}
