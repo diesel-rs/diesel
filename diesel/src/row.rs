ae80cc56003527d837a8c1cda8769ab75ba72486
Sean Griffin <sean@seantheprogrammer.com>
2017-08-03 14:57:38 -0400

    Document the `row` module

--- diesel/src/row.rs
+++ diesel/src/row.rs
@@ -1,3 +1,5 @@
+//! Contains the `Row` trait
+
 use backend::Backend;
 
 /// The row trait which is used for [`FromSqlRow`][]. Apps should not need to
@@ -5,9 +7,15 @@ use backend::Backend;
 ///
 /// [`FromSqlRow`]: ../types/trait.FromSqlRow.html
 pub trait Row<DB: Backend> {
+    /// Returns the value of the next column in the row.
     fn take(&mut self) -> Option<&DB::RawValue>;
+
+    /// Returns whether the next `count` columns are all `NULL`.
     fn next_is_null(&self, count: usize) -> bool;
 
+    /// Skips the next `count` columns. This method must be called if you are
+    /// choosing not to call `take` as a result of `next_is_null` returning
+    /// `true`.
     fn advance(&mut self, count: usize) {
         for _ in 0..count {
             self.take();
