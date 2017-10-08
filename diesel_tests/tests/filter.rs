diesel-rs/diesel

commit c85965db2eb638ea61eac6622b26260588b6c3d8

Allow subselects to reference columns from the outer table (#1202)
This changes the semantics of `exists`, and the various array_comparison
functions/methods to allow the subselect passed to them to reference
columns from the outer table. In other words, this allows subselects
which are not valid queries on their own, but are valid expressions in
this context.

Note that this only allows the where clause to reference the outer
table, as it's much less useful to do so from elsewhere in the query.

There are a few side effects of this change that will need to be
addressed later on. The first is that `exists` can now take any
expression, not just select statements. We should limit this to
`BoxedSelectStatement` and `SelectStatement`. Second is that you can no
longer pass a subselect to the same table. I'm not entirely sure how we
go about fixing this just yet, but that's a much more niche use case
anyway.

There is still additional work to be done to make this useable though,
as it will still be impossible to construct many queries while most of
our DSL methods still have `AsQuery` bounds. They will need to be
loosened in a similar fashion to #1200.

diesel_tests/tests/filter.rs
@@ -386,3 +386,60 @@ fn filter_by_boxed_predicate() {
     assert_eq!(Ok(sean), queried_sean);
     assert_eq!(Ok(tess), queried_tess);
 }
+
+#[test]
+fn filter_subselect_referencing_outer_table() {
+    use diesel::dsl::exists;
+
+    let conn = connection_with_sean_and_tess_in_users_table();
+    let sean = find_user_by_name("Sean", &conn);
+
+    let new_posts = vec![sean.new_post("Hello", None), sean.new_post("Hello 2", None)];
+    insert(&new_posts)
+        .into(posts::table)
+        .execute(&conn)
+        .unwrap();
+
+    let expected = Ok(vec![sean]);
+    let users_with_published_posts = users::table
+        .filter(exists(posts::table.filter(posts::user_id.eq(users::id))))
+        .load(&conn);
+    assert_eq!(expected, users_with_published_posts);
+
+    let users_with_published_posts = users::table
+        .filter(
+            users::id.eq_any(
+                posts::table
+                    .select(posts::user_id)
+                    .filter(posts::user_id.eq(users::id)),
+            ),
+        )
+        .load(&conn);
+    assert_eq!(expected, users_with_published_posts);
+}
+
+#[test]
+#[cfg(feature = "postgres")]
+fn filter_subselect_with_pg_any() {
+    use diesel::dsl::any;
+
+    let conn = connection_with_sean_and_tess_in_users_table();
+    let sean = find_user_by_name("Sean", &conn);
+
+    let new_posts = vec![sean.new_post("Hello", None), sean.new_post("Hello 2", None)];
+    insert(&new_posts)
+        .into(posts::table)
+        .execute(&conn)
+        .unwrap();
+
+    let users_with_published_posts = users::table
+        .filter(
+            users::id.eq(any(
+                posts::table
+                    .select(posts::user_id)
+                    .filter(posts::user_id.eq(users::id)),
+            )),
+        )
+        .load(&conn);
+    assert_eq!(Ok(vec![sean]), users_with_published_posts);
+}

diesel/src/expression/array_comparison.rs
@@ -139,7 +139,6 @@ pub trait MaybeEmpty {
 
 impl<ST, S, F, W, O, L, Of, G, FU> AsInExpression<ST> for SelectStatement<S, F, W, O, L, Of, G, FU>
 where
-    Self: Query<SqlType = ST>,
     Subselect<Self, ST>: Expression<SqlType = ST>,
 {
     type InExpression = Subselect<Self, ST>;
@@ -221,7 +220,7 @@ pub struct Subselect<T, ST> {
     _sql_type: PhantomData<ST>,
 }
 
-impl<T: Query, ST> Expression for Subselect<T, ST> {
+impl<T: Expression, ST> Expression for Subselect<T, ST> {
     type SqlType = ST;
 }
 
@@ -234,14 +233,14 @@ impl<T, ST> MaybeEmpty for Subselect<T, ST> {
 impl<T, ST, QS> SelectableExpression<QS> for Subselect<T, ST>
 where
     Subselect<T, ST>: AppearsOnTable<QS>,
-    T: Query,
+    T: SelectableExpression<QS>,
 {
 }
 
 impl<T, ST, QS> AppearsOnTable<QS> for Subselect<T, ST>
 where
     Subselect<T, ST>: Expression,
-    T: Query,
+    T: AppearsOnTable<QS>,
 {
 }
 

diesel/src/expression/exists.rs
@@ -36,8 +36,8 @@ use types::Bool;
 /// assert_eq!(Ok(false), jim_exists);
 /// # }
 /// ```
-pub fn exists<T: AsQuery>(query: T) -> Exists<T::Query> {
-    Exists(query.as_query())
+pub fn exists<T>(query: T) -> Exists<T> {
+    Exists(query)
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -45,7 +45,7 @@ pub struct Exists<T>(T);
 
 impl<T> Expression for Exists<T>
 where
-    T: Query,
+    T: Expression,
 {
     type SqlType = Bool;
 }

diesel/src/macros/mod.rs
@@ -745,6 +745,10 @@ macro_rules! table_body {
                 type Count = Once;
             }
 
+            impl AppearsInFromClause<table> for () {
+                type Count = Never;
+            }
+
             impl<T> AppearsInFromClause<T> for table where
                 T: Table + JoinTo<table>,
             {
@@ -1103,6 +1107,7 @@ mod tests {
     }
 
     mod my_types {
+        #[derive(Debug, Clone, Copy)]
         pub struct MyCustomType;
     }
 

diesel/src/query_builder/select_statement/dsl_impls.rs
@@ -85,12 +85,10 @@ where
     }
 }
 
-impl<ST, F, S, D, W, O, L, Of, G, FU, Predicate> FilterDsl<Predicate>
+impl<F, S, D, W, O, L, Of, G, FU, Predicate> FilterDsl<Predicate>
     for SelectStatement<F, S, D, W, O, L, Of, G, FU>
 where
-    Self: AsQuery<SqlType = ST>,
-    SelectStatement<F, S, D, W::Output, O, L, Of, G, FU>: Query<SqlType = ST>,
-    Predicate: AppearsOnTable<F, SqlType = Bool> + NonAggregate,
+    Predicate: Expression<SqlType = Bool> + NonAggregate,
     W: WhereAnd<Predicate>,
 {
     type Output = SelectStatement<F, S, D, W::Output, O, L, Of, G, FU>;
@@ -327,6 +325,7 @@ where
 impl<F, W> IntoUpdateTarget for SelectStatement<F, DefaultSelectClause, NoDistinctClause, W>
 where
     SelectStatement<F, DefaultSelectClause, NoDistinctClause, W>: HasTable,
+    W: ValidWhereClause<F>,
 {
     type WhereClause = W;
 

diesel/src/query_builder/select_statement/mod.rs
@@ -17,7 +17,7 @@ pub use self::boxed::BoxedSelectStatement;
 use backend::Backend;
 use expression::*;
 use query_source::*;
-use query_source::joins::AppendSelection;
+use query_source::joins::{AppendSelection, Inner, Join};
 use result::QueryResult;
 use super::distinct_clause::NoDistinctClause;
 use super::for_update_clause::NoForUpdateClause;
@@ -26,7 +26,7 @@ use super::limit_clause::NoLimitClause;
 use super::offset_clause::NoOffsetClause;
 use super::order_clause::NoOrderClause;
 use super::select_clause::*;
-use super::where_clause::NoWhereClause;
+use super::where_clause::*;
 use super::{AstPass, Query, QueryFragment};
 
 #[derive(Debug, Clone, Copy)]
@@ -100,6 +100,7 @@ impl<F> SelectStatement<F> {
 impl<F, S, D, W, O, L, Of, G, FU> Query for SelectStatement<F, S, D, W, O, L, Of, G, FU>
 where
     S: SelectClauseExpression<F>,
+    W: ValidWhereClause<F>,
 {
     type SqlType = S::SelectClauseSqlType;
 }
@@ -188,9 +189,10 @@ where
 }
 
 impl<S, F, D, W, O, L, Of, G, FU, QS> AppearsOnTable<QS>
-    for SelectStatement<S, F, D, W, O, L, Of, FU, G>
+    for SelectStatement<F, S, D, W, O, L, Of, FU, G>
 where
     Self: Expression,
+    W: ValidWhereClause<Join<F, QS, Inner>>,
 {
 }
 

diesel/src/query_builder/where_clause.rs
@@ -78,3 +78,15 @@ where
         Some(Box::new(self.0))
     }
 }
+
+/// Marker trait indicating that a `WHERE` clause is valid for a given query
+/// source.
+pub trait ValidWhereClause<QS> {}
+
+impl<QS> ValidWhereClause<QS> for NoWhereClause {}
+
+impl<QS, Expr> ValidWhereClause<QS> for WhereClause<Expr>
+where
+    Expr: AppearsOnTable<QS>,
+{
+}

diesel_compile_tests/tests/compile-fail/any_is_only_selectable_if_inner_expr_is_selectable.rs
@@ -1,5 +1,5 @@
-#[macro_use]
-extern crate diesel;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
 
 use diesel::*;
 use diesel::dsl::*;
@@ -17,10 +17,21 @@ table! {
     }
 }
 
+#[derive(Queryable)]
+struct Stuff {
+    id: i32,
+    name: String,
+}
+
 fn main() {
     use self::stuff::dsl::*;
 
-    stuff.filter(name.eq(any(more_stuff::names)));
+    let conn = PgConnection::establish("").unwrap();
+
+    let _ = LoadDsl::load::<Stuff>(
     //~^ ERROR E0277
     //~| ERROR E0271
+        stuff.filter(name.eq(any(more_stuff::names))),
+        &conn,
+    );
 }

commit 016aeedf22e58655a2d7707d6506f2441951bfc9

Fix passing boxed queries as subselects
This functionality was broken in c85965db as `BoxedSelectStatement` did
not implement `Expression`. I've implemented `AppearsOnTable` for all
possible query sources, since a boxed select statement must only
reference its own table by design.

diesel/src/query_builder/select_statement/boxed.rs
@@ -62,6 +62,19 @@ where
     type SqlType = ST;
 }
 
+impl<'a, ST, QS, DB> Expression for BoxedSelectStatement<'a, ST, QS, DB>
+where
+    Self: Query<SqlType = ST>,
+{
+    type SqlType = ST;
+}
+
+impl<'a, ST, QS, QS2, DB> AppearsOnTable<QS2> for BoxedSelectStatement<'a, ST, QS, DB>
+where
+    Self: Query<SqlType = ST>,
+{
+}
+
 impl<'a, ST, QS, DB> QueryFragment<DB> for BoxedSelectStatement<'a, ST, QS, DB>
 where
     DB: Backend,

diesel_tests/tests/filter.rs
@@ -420,6 +420,20 @@ fn filter_subselect_referencing_outer_table() {
 }
 
 #[test]
+fn filter_subselect_with_boxed_query() {
+    use schema::users::dsl::*;
+
+    let conn = connection_with_sean_and_tess_in_users_table();
+    let sean = find_user_by_name("Sean", &conn);
+
+    let subselect = users.filter(name.eq("Sean")).select(id).into_boxed();
+
+    let expected = Ok(vec![sean]);
+    let data = users.filter(id.eq_any(subselect)).load(&conn);
+    assert_eq!(expected, data);
+}
+
+#[test]
 #[cfg(feature = "postgres")]
 fn filter_subselect_with_pg_any() {
     use diesel::dsl::any;