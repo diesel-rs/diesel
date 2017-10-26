f7eba6a256a79ba69a99debf45e3a469a279f714
Sean Griffin <sean@seantheprogrammer.com>
2017-10-26 13:46:08 -0600

    Document all the items of `IntoUpdateTarget`

--- diesel/src/query_builder/update_statement/target.rs
+++ diesel/src/query_builder/update_statement/target.rs
@@ -13,8 +13,8 @@ pub struct UpdateTarget<Table, WhereClause> {
 /// A type which can be passed to `update`.
 ///
 /// Apps will never need to implement this type directly. There are three kinds
-/// of things which implement this trait. Tables, queries which have only had
-/// `filter` called on them, and types which implement `Identifiable`.
+/// which implement this trait. Tables, queries which have only had `filter`
+/// called on them, and types which implement `Identifiable`.
 ///
 /// When a table is passed to `update`, every row in the table will be updated.
 /// You can scope this down by calling `filter` before passing it, which will
@@ -22,8 +22,10 @@ pub struct UpdateTarget<Table, WhereClause> {
 /// which implements `Identifiable` is the same as passing
 /// `SomeStruct::table().find(some_struct)`.
 pub trait IntoUpdateTarget: HasTable {
+    /// What is the `WHERE` clause of this target?
     type WhereClause;
 
+    /// Decomposes `self` into the table and where clause.
     fn into_update_target(self) -> UpdateTarget<Self::Table, Self::WhereClause>;
 }
 
