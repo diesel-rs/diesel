0432e09c7db08e7a51959f6a9f58fd9815f4438d
Sean Griffin <sean@seantheprogrammer.com>
2018-01-02 14:47:50 -0700

    Merge pull request #1428 from diesel-rs/sg-advanced-blog-cli
    Add a more "advanced" example

---
+++ examples/postgres/advanced-blog-cli/src/schema.rs
@@ -0,0 +1,38 @@
+table! {
+    comments (id) {
+        id -> Int4,
+        user_id -> Int4,
+        post_id -> Int4,
+        body -> Text,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
+    posts (id) {
+        id -> Int4,
+        user_id -> Int4,
+        title -> Text,
+        body -> Text,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        published_at -> Nullable<Timestamp>,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        username -> Text,
+        hashed_password -> Text,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+joinable!(comments -> posts (post_id));
+joinable!(comments -> users (user_id));
+joinable!(posts -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(comments, posts, users,);
