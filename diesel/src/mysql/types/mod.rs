6aa9f48f439ba0377557c1fa753c3df696b8c4b3
Sean Griffin <sean@seantheprogrammer.com>
2018-01-06 08:49:47 -0700

    Fix failing build

--- diesel/src/mysql/types/mod.rs
+++ diesel/src/mysql/types/mod.rs
@@ -6,8 +6,6 @@ mod numeric;
 
 use byteorder::WriteBytesExt;
 use mysql::{Mysql, MysqlType};
-#[cfg(not(feature = "postgres"))]
-use query_builder::QueryId;
 use std::error::Error as StdError;
 use std::io::Write;
 use types::{FromSql, HasSqlType, IsNull, Tinyint, ToSql, ToSqlOutput};
