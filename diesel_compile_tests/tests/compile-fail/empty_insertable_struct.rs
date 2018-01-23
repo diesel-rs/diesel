2186b1309a11f72c66341b15279742a0c429cfb2
Georg Semmler <georg_semmler_05@web.de>
2018-01-23 10:31:32 +0100

    Update error messages in compile-fail tests

--- diesel_compile_tests/tests/compile-fail/empty_insertable_struct.rs
+++ diesel_compile_tests/tests/compile-fail/empty_insertable_struct.rs
@@ -9,8 +9,7 @@ table! {
 }
 
 #[derive(Insertable)]
-//~^ ERROR proc-macro derive panicked
-//~| HELP Failed to derive `Insertable` for `NewUser`: `Insertable` cannot be used on structs with empty fields
+//~^ ERROR Failed to derive `Insertable` for `NewUser`: `Insertable` cannot be used on structs with empty fields
 #[table_name="users"]
 pub struct NewUser {}
 
