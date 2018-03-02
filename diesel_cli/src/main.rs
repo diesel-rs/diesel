c4cee3da980fc890c77a53de02bff85462f6d06b
Katharina Fey <kookie@spacekookie.de>
2018-03-02 09:29:54 +0100

    Changing the migration generation function signature

--- diesel_cli/src/main.rs
+++ diesel_cli/src/main.rs
@@ -118,14 +118,14 @@ fn run_migration_command(matches: &ArgMatches) {
             match migration_type {
                 #[cfg(feature = "barrel")]
                 Some("barrel") => barrel::diesel::generate_initial(migration_dir),
-                _ => generate_sql_migration(migration_dir),
+                _ => generate_sql_migration(&migration_dir),
             }
         }
         _ => unreachable!("The cli parser should prevent reaching here"),
     }
 }
 
-fn generate_sql_migration(path: PathBuf) {
+fn generate_sql_migration(path: &PathBuf) {
     use std::io::Write;
 
     let migration_dir_relative =
