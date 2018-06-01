934bc1fab145fd9e9b3679cab568bf81ed8bcffd
Sean Griffin <sean@seantheprogrammer.com>
2018-06-01 11:56:57 -0600

    Remove unused import from `embed_migrations!`
    The changes for barrel support led to `diesel_migrations` exporting
    everything required by this macro, leading to the warning being
    generated.
    
    Fixes #1739

--- diesel_migrations/migrations_macros/src/embed_migrations.rs
+++ diesel_migrations/migrations_macros/src/embed_migrations.rs
@@ -66,7 +66,6 @@ pub fn derive_embed_migrations(input: &syn::DeriveInput) -> quote::Tokens {
         extern crate diesel_migrations;
 
         use self::diesel_migrations::*;
-        use self::diesel::migration::*;
         use self::diesel::connection::SimpleConnection;
         use std::io;
 
