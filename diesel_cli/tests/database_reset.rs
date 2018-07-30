e14c8af2ef6f20323211c0c2524674eb0fee0960
Dominik Sander <git@dsander.de>
2018-07-30 11:08:23 +0200

    Indent `diesel.toml` contents like the other `file` arguments.

--- diesel_cli/tests/database_reset.rs
+++ diesel_cli/tests/database_reset.rs
@@ -178,9 +178,9 @@ fn reset_updates_schema_if_config_present() {
         .file(
             "diesel.toml",
             r#"
-[print_schema]
-file = "src/my_schema.rs"
-"#,
+            [print_schema]
+            file = "src/my_schema.rs"
+            "#,
         )
         .build();
 
