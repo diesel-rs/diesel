1de3ec21d66df30ecba0f8d2725030b4f5bbe055
Hirokazu Hata <h.hata.ai.t@gmail.com>
2018-11-08 01:38:30 +0900

    Add line break

--- diesel/src/upsert/mod.rs
+++ diesel/src/upsert/mod.rs
@@ -1,6 +1,6 @@
 //! Types and functions related to PG's and Sqlite's `ON CONFLICT` clause
-//! If you use upsert, you need use PostgreSQL since version 9.0 or Sqlite3 since version 3.24.0.
 //!
+//! If you use upsert, you need use PostgreSQL since version 9.0 or Sqlite3 since version 3.24.0.
 //! See [the methods on `InsertStatement`](../../query_builder/struct.InsertStatement.html#impl-1)
 //! for usage examples.
 
