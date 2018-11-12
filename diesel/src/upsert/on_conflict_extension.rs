fa958e51d13566009f3e29bfc01442c1acc51f67
Hirokazu Hata <h.hata.ai.t@gmail.com>
2018-11-12 09:50:25 +0900

    Hide cfg attribute

--- diesel/src/upsert/on_conflict_extension.rs
+++ diesel/src/upsert/on_conflict_extension.rs
@@ -51,15 +51,15 @@ where
     /// #     let conn = establish_connection();
     /// #     #[cfg(feature = "postgres")]
     /// #     conn.execute("TRUNCATE TABLE users").unwrap();
-    /// #[cfg(feature = "postgres")]
+    /// # #[cfg(feature = "postgres")]
     /// let user = User { id: 1, name: "Sean", };
     ///
-    /// #[cfg(feature = "postgres")]
+    /// # #[cfg(feature = "postgres")]
     /// let inserted_row_count = diesel::insert_into(users)
     ///     .values(&vec![user, user])
     ///     .on_conflict_do_nothing()
     ///     .execute(&conn);
-    /// #[cfg(feature = "postgres")]
+    /// # #[cfg(feature = "postgres")]
     /// assert_eq!(Ok(1), inserted_row_count);
     /// # }
     /// ```
@@ -296,7 +296,7 @@ impl<Stmt, Target> IncompleteOnConflict<Stmt, Target> {
     /// let user2 = User { id: 1, name: "Sean" };
     /// let user3 = User { id: 2, name: "Tess" };
     ///
-    /// #[cfg(feature = "postgres")]
+    /// # #[cfg(feature = "postgres")]
     /// assert_eq!(Ok(1), diesel::insert_into(users).values(&user).execute(&conn));
     ///
     /// #[cfg(feature = "postgres")]
@@ -306,12 +306,12 @@ impl<Stmt, Target> IncompleteOnConflict<Stmt, Target> {
     ///     .do_update()
     ///     .set(name.eq(excluded(name)))
     ///     .execute(&conn);
-    /// #[cfg(feature = "postgres")]
+    /// # #[cfg(feature = "postgres")]
     /// assert_eq!(Ok(2), insert_count);
     ///
-    /// #[cfg(feature = "postgres")]
+    /// # #[cfg(feature = "postgres")]
     /// let users_in_db = users.load(&conn);
-    /// #[cfg(feature = "postgres")]
+    /// # #[cfg(feature = "postgres")]
     /// assert_eq!(Ok(vec![(1, "Sean".to_string()), (2, "Tess".to_string())]), users_in_db);
     /// # }
     /// ```
