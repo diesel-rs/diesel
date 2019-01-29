cc559ab301f032da8aaccbd50f8f1af5bce4124a
Dmitry Shlagoff <richyannet@yandex.ru>
2019-01-29 18:31:03 +0700

    Add impls for ranges from std + refactor a bit

--- diesel/src/pg/types/ranges.rs
+++ diesel/src/pg/types/ranges.rs
@@ -1,6 +1,7 @@
 use byteorder::{NetworkEndian, ReadBytesExt, WriteBytesExt};
 use std::collections::Bound;
 use std::io::Write;
+use std::ops::{Range, RangeFrom, RangeInclusive, RangeToInclusive};
 
 use deserialize::{self, FromSql, FromSqlRow, Queryable};
 use expression::bound::Bound as SqlBound;
@@ -23,56 +24,73 @@ bitflags! {
     }
 }
 
-impl<T, ST> Queryable<SqlRange<ST>, Pg> for (Bound<T>, Bound<T>)
-where
-    T: FromSql<ST, Pg> + Queryable<ST, Pg>,
-{
-    type Row = Self;
-    fn build(row: Self) -> Self {
-        row
-    }
+macro_rules! setup_impls {
+    ($target:ty) => {
+        impl<T, ST> Queryable<SqlRange<ST>, Pg> for $target
+        where
+            T: FromSql<ST, Pg> + Queryable<ST, Pg>,
+        {
+            type Row = Self;
+            fn build(row: Self) -> Self {
+                row
+            }
+        }
+
+        impl<ST, T> AsExpression<SqlRange<ST>> for $target {
+            type Expression = SqlBound<SqlRange<ST>, Self>;
+
+            fn as_expression(self) -> Self::Expression {
+                SqlBound::new(self)
+            }
+        }
+
+        impl<'a, ST, T> AsExpression<SqlRange<ST>> for &'a $target {
+            type Expression = SqlBound<SqlRange<ST>, Self>;
+
+            fn as_expression(self) -> Self::Expression {
+                SqlBound::new(self)
+            }
+        }
+
+        impl<ST, T> AsExpression<Nullable<SqlRange<ST>>> for $target {
+            type Expression = SqlBound<Nullable<SqlRange<ST>>, Self>;
+
+            fn as_expression(self) -> Self::Expression {
+                SqlBound::new(self)
+            }
+        }
+
+        impl<'a, ST, T> AsExpression<Nullable<SqlRange<ST>>> for &'a $target {
+            type Expression = SqlBound<Nullable<SqlRange<ST>>, Self>;
+
+            fn as_expression(self) -> Self::Expression {
+                SqlBound::new(self)
+            }
+        }
+
+        impl<T, ST> FromSqlRow<SqlRange<ST>, Pg> for $target
+        where
+            $target: FromSql<SqlRange<ST>, Pg>,
+        {
+            fn build_from_row<R: ::row::Row<Pg>>(row: &mut R) -> deserialize::Result<Self> {
+                FromSql::<SqlRange<ST>, Pg>::from_sql(row.take())
+            }
+        }
+
+        impl<ST, T> ToSql<Nullable<SqlRange<ST>>, Pg> for $target
+        where
+            $target: ToSql<SqlRange<ST>, Pg>,
+        {
+            fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+                ToSql::<SqlRange<ST>, Pg>::to_sql(self, out)
+            }
+        }
+    };
 }
 
-impl<ST, T> AsExpression<SqlRange<ST>> for (Bound<T>, Bound<T>) {
-    type Expression = SqlBound<SqlRange<ST>, Self>;
+// (Bound<T>, Bound<T>) as range representation
 
-    fn as_expression(self) -> Self::Expression {
-        SqlBound::new(self)
-    }
-}
-
-impl<'a, ST, T> AsExpression<SqlRange<ST>> for &'a (Bound<T>, Bound<T>) {
-    type Expression = SqlBound<SqlRange<ST>, Self>;
-
-    fn as_expression(self) -> Self::Expression {
-        SqlBound::new(self)
-    }
-}
-
-impl<ST, T> AsExpression<Nullable<SqlRange<ST>>> for (Bound<T>, Bound<T>) {
-    type Expression = SqlBound<Nullable<SqlRange<ST>>, Self>;
-
-    fn as_expression(self) -> Self::Expression {
-        SqlBound::new(self)
-    }
-}
-
-impl<'a, ST, T> AsExpression<Nullable<SqlRange<ST>>> for &'a (Bound<T>, Bound<T>) {
-    type Expression = SqlBound<Nullable<SqlRange<ST>>, Self>;
-
-    fn as_expression(self) -> Self::Expression {
-        SqlBound::new(self)
-    }
-}
-
-impl<T, ST> FromSqlRow<SqlRange<ST>, Pg> for (Bound<T>, Bound<T>)
-where
-    (Bound<T>, Bound<T>): FromSql<SqlRange<ST>, Pg>,
-{
-    fn build_from_row<R: ::row::Row<Pg>>(row: &mut R) -> deserialize::Result<Self> {
-        FromSql::<SqlRange<ST>, Pg>::from_sql(row.take())
-    }
-}
+setup_impls! {(Bound<T>, Bound<T>)}
 
 impl<T, ST> FromSql<SqlRange<ST>, Pg> for (Bound<T>, Bound<T>)
 where
@@ -157,65 +175,156 @@ where
     }
 }
 
-impl<ST, T> ToSql<Nullable<SqlRange<ST>>, Pg> for (Bound<T>, Bound<T>)
+macro_rules! bounds_err {
+    ($name:ident, $expected_lower:ident, $expected_upper:ident, $got:expr) => {
+        Err(format!(
+            "Unexpected bounds for {}. Expected bounds to be {}..{}, got $got",
+            "$name", "$expected_lower", "$expected_upper"
+        )
+        .into())
+    };
+}
+
+// `std::ops::Range`
+
+setup_impls! {Range<T>}
+
+impl<T, ST> FromSql<SqlRange<ST>, Pg> for Range<T>
 where
-    (Bound<T>, Bound<T>): ToSql<SqlRange<ST>, Pg>,
+    T: FromSql<ST, Pg>,
+{
+    fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+        let (lower_bound, upper_bound) = FromSql::from_sql(bytes)?;
+
+        match (lower_bound, upper_bound) {
+            (Bound::Included(start), Bound::Excluded(end)) => Ok(Self { start, end }),
+            _erroneous_bounds => bounds_err!(Range, Included, Excluded, _erroneous_bounds),
+        }
+    }
+}
+
+impl<ST, T> ToSql<SqlRange<ST>, Pg> for Range<T>
+where
+    T: ToSql<ST, Pg>,
 {
     fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
-        ToSql::<SqlRange<ST>, Pg>::to_sql(self, out)
+        let range = (Bound::Included(&self.start), Bound::Excluded(&self.end));
+        ToSql::<SqlRange<ST>, Pg>::to_sql(&range, out)?;
+
+        Ok(IsNull::No)
     }
 }
 
-impl HasSqlType<Int4range> for Pg {
-    fn metadata(_: &PgMetadataLookup) -> PgTypeMetadata {
-        PgTypeMetadata {
-            oid: 3904,
-            array_oid: 3905,
+// `std::ops::RangeFrom`
+
+setup_impls! {RangeFrom<T>}
+
+impl<T, ST> FromSql<SqlRange<ST>, Pg> for RangeFrom<T>
+where
+    T: FromSql<ST, Pg>,
+{
+    fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+        let (lower_bound, upper_bound) = FromSql::from_sql(bytes)?;
+
+        match (lower_bound, upper_bound) {
+            (Bound::Included(start), Bound::Unbounded) => Ok(Self { start }),
+            _erroneous_bounds => bounds_err!(Range, Included, Unbounded, _erroneous_bounds),
         }
     }
 }
 
-impl HasSqlType<Numrange> for Pg {
-    fn metadata(_: &PgMetadataLookup) -> PgTypeMetadata {
-        PgTypeMetadata {
-            oid: 3906,
-            array_oid: 3907,
+impl<ST, T> ToSql<SqlRange<ST>, Pg> for RangeFrom<T>
+where
+    T: ToSql<ST, Pg>,
+{
+    fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+        let range = (Bound::Included(&self.start), Bound::Unbounded);
+        ToSql::<SqlRange<ST>, Pg>::to_sql(&range, out)?;
+
+        Ok(IsNull::No)
+    }
+}
+
+// `std::ops::RangeInclusive`
+
+setup_impls! {RangeInclusive<T>}
+
+impl<T, ST> FromSql<SqlRange<ST>, Pg> for RangeInclusive<T>
+where
+    T: FromSql<ST, Pg>,
+{
+    fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+        let (lower_bound, upper_bound) = FromSql::from_sql(bytes)?;
+
+        match (lower_bound, upper_bound) {
+            (Bound::Included(start), Bound::Included(end)) => Ok(Self::new(start, end)),
+            _erroneous_bounds => bounds_err!(Range, Included, Included, _erroneous_bounds),
         }
     }
 }
 
-impl HasSqlType<Tsrange> for Pg {
-    fn metadata(_: &PgMetadataLookup) -> PgTypeMetadata {
-        PgTypeMetadata {
-            oid: 3908,
-            array_oid: 3909,
+impl<ST, T> ToSql<SqlRange<ST>, Pg> for RangeInclusive<T>
+where
+    T: ToSql<ST, Pg>,
+{
+    fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+        let start = self.start();
+        let end = self.end();
+        let range = (Bound::Included(start), Bound::Included(end));
+        ToSql::<SqlRange<ST>, Pg>::to_sql(&range, out)?;
+
+        Ok(IsNull::No)
+    }
+}
+
+// `std::ops::RangeToInclusive`
+
+setup_impls! {RangeToInclusive<T>}
+
+impl<T, ST> FromSql<SqlRange<ST>, Pg> for RangeToInclusive<T>
+where
+    T: FromSql<ST, Pg>,
+{
+    fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+        let (lower_bound, upper_bound) = FromSql::from_sql(bytes)?;
+
+        match (lower_bound, upper_bound) {
+            (Bound::Unbounded, Bound::Included(end)) => Ok(Self { end }),
+            _erroneous_bounds => bounds_err!(Range, Unbounded, Included, _erroneous_bounds),
         }
     }
 }
 
-impl HasSqlType<Tstzrange> for Pg {
-    fn metadata(_: &PgMetadataLookup) -> PgTypeMetadata {
-        PgTypeMetadata {
-            oid: 3910,
-            array_oid: 3911,
-        }
+impl<ST, T> ToSql<SqlRange<ST>, Pg> for RangeToInclusive<T>
+where
+    T: ToSql<ST, Pg>,
+{
+    fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+        let range = (Bound::Unbounded, Bound::Included(&self.end));
+        ToSql::<SqlRange<ST>, Pg>::to_sql(&range, out)?;
+
+        Ok(IsNull::No)
     }
 }
 
-impl HasSqlType<Daterange> for Pg {
-    fn metadata(_: &PgMetadataLookup) -> PgTypeMetadata {
-        PgTypeMetadata {
-            oid: 3912,
-            array_oid: 3913,
+// Built-in PG ranges
+
+macro_rules! impl_has_sql_type {
+    ($target:ty, $oid:expr, $array_oid:expr) => {
+        impl HasSqlType<$target> for Pg {
+            fn metadata(_: &PgMetadataLookup) -> PgTypeMetadata {
+                PgTypeMetadata {
+                    oid: $oid,
+                    array_oid: $array_oid,
+                }
+            }
         }
-    }
+    };
 }
 
-impl HasSqlType<Int8range> for Pg {
-    fn metadata(_: &PgMetadataLookup) -> PgTypeMetadata {
-        PgTypeMetadata {
-            oid: 3926,
-            array_oid: 3927,
-        }
-    }
-}
+impl_has_sql_type! {Int4range, 3904, 3905}
+impl_has_sql_type! {Numrange, 3906, 3907}
+impl_has_sql_type! {Tsrange, 3908, 3909}
+impl_has_sql_type! {Tstzrange, 3910, 3911}
+impl_has_sql_type! {Daterange, 3912, 3913}
+impl_has_sql_type! {Int8range, 3926, 3927}
