52d6c9e107d34512a858608f3b2891a600a7b39f
Bastien Orivel <eijebong@bananium.fr>
2019-02-23 17:27:16 +0100

    Update syn to 0.15 in the migrations_macros crate

--- diesel_migrations/migrations_macros/src/lib.rs
+++ diesel_migrations/migrations_macros/src/lib.rs
@@ -23,8 +23,10 @@
 #![cfg_attr(test, allow(clippy::option_unwrap_used, clippy::result_unwrap_used))]
 extern crate migrations_internals;
 extern crate proc_macro;
+extern crate proc_macro2;
 #[macro_use]
 extern crate quote;
+#[macro_use]
 extern crate syn;
 
 mod embed_migrations;
@@ -32,11 +34,11 @@ mod migrations;
 mod util;
 
 use proc_macro::TokenStream;
-use syn::parse_derive_input;
+use syn::DeriveInput;
 
 #[proc_macro_derive(EmbedMigrations, attributes(embed_migrations_options))]
 pub fn derive_embed_migrations(input: TokenStream) -> TokenStream {
-    let item = parse_derive_input(&input.to_string()).unwrap();
+    let item = parse_macro_input!(input as DeriveInput);
     embed_migrations::derive_embed_migrations(&item)
         .to_string()
         .parse()
