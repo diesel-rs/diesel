1cc4a03e100a4631dfd3648c2cc894fac0c266f7
Georg Semmler <georg_semmler_05@web.de>
2019-04-14 15:14:51 +0200

    More fixes

--- diesel_cli/src/database.rs
+++ diesel_cli/src/database.rs
@@ -234,9 +234,6 @@ fn drop_database(database_url: &str) -> DatabaseResult<()> {
         }
         #[cfg(feature = "sqlite")]
         Backend::Sqlite => {
-            use std::fs;
-            use std::path::Path;
-
             if Path::new(database_url).exists() {
                 println!("Dropping database: {}", database_url);
                 fs::remove_file(&database_url)?;
