036f0ed674aa7ab1891f9298126c71c93b56eec6
Ryan Leckey <ryan@launchbadge.com>
2019-06-03 00:25:40 -0700

    Run rustfmt

--- diesel/src/query_builder/select_statement/boxed.rs
+++ diesel/src/query_builder/select_statement/boxed.rs
@@ -40,17 +40,10 @@ impl<'a, ST, QS, DB> BoxedSelectStatement<'a, ST, QS, DB> {
         from: QS,
         distinct: Box<dyn QueryFragment<DB> + Send + 'a>,
         where_clause: BoxedWhereClause<'a, DB>,
-<<<<<<< HEAD
         order: Option<Box<dyn QueryFragment<DB> + Send + 'a>>,
         limit: Box<dyn QueryFragment<DB> + Send + 'a>,
         offset: Box<dyn QueryFragment<DB> + Send + 'a>,
         group_by: Box<dyn QueryFragment<DB> + Send + 'a>,
-=======
-        order: Option<Box<QueryFragment<DB> + Send + 'a>>,
-        limit: Box<QueryFragment<DB> + Send + 'a>,
-        offset: Box<QueryFragment<DB> + Send + 'a>,
-        group_by: Box<QueryFragment<DB> + Send + 'a>,
->>>>>>> Run rustfmt
     ) -> Self {
         BoxedSelectStatement {
             select: select,
@@ -350,8 +343,8 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::prelude::*;
     use crate::backend::Backend;
+    use crate::prelude::*;
 
     table! {
         users {
@@ -359,7 +352,11 @@ mod tests {
         }
     }
 
-    fn assert_send<T>(_: T) where T: Send {}
+    fn assert_send<T>(_: T)
+    where
+        T: Send,
+    {
+    }
 
     fn assert_boxed_query_send<B: Backend>() {
         assert_send(users::table.into_boxed::<B>());
