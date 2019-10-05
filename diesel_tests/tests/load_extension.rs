f875697000bd74085519c6573f1072e9fa924fa6
alamminsalo <antti.lamminsalo@protonmail.com>
2019-10-05 14:48:55 +0300

    stop trying to load spatialite, as this proved to be a big hassle on all platforms. Now just tests if error message changes from not authorized

--- diesel_tests/tests/load_extension.rs
+++ diesel_tests/tests/load_extension.rs
@@ -5,64 +5,26 @@ fn conn() -> SqliteConnection {
 }
 
 #[test]
-fn test_load_extension_fail() {
+fn test_load_extension() {
     let conn = conn();
 
     // disable extension loading
     conn.enable_load_extension(false).unwrap();
 
     // try loading module without enabling extension loading
-    let result = sql_query("SELECT load_extension('mod_spatialite');").execute(&conn);
-
-    assert!(result.is_err());
-}
-
-#[test]
-fn test_load_extension_ok() {
-    let conn = conn();
-
-    // enable loading
-    conn.enable_load_extension(true).unwrap();
-
-    // load mod_spatialite
-    let result = sql_query("SELECT load_extension('mod_spatialite');").execute(&conn);
-
-    assert!(result.is_ok());
-}
-
-table! {
-    foo {
-        id -> Integer,
-        bar -> VarChar,
-    }
-}
-
-#[derive(QueryableByName)]
-#[table_name = "foo"]
-struct Foo {
-    id: i32,
-    bar: String,
-}
-
-#[test]
-fn test_extension_function() {
-    let conn = conn();
-
-    // enable loading
-    conn.enable_load_extension(true).unwrap();
-
-    // load mod_spatialite
-    sql_query("SELECT load_extension('mod_spatialite');")
+    let result = sql_query("SELECT load_extension('/tmp/foo');")
         .execute(&conn)
-        .expect("Failed to load mod_spatialite.so");
+        .expect_err("should have been error");
 
-    // test module function
-    let r: Vec<Foo> =
-        sql_query("SELECT * FROM (SELECT 0 as id, AsText(ST_Point(25.2,54.2)) as bar) foo;")
-            .load(&conn)
-            .expect("Failed to query lib version");
+    assert_eq!(&format!("{}", &result), "not authorized");
 
-    for v in r {
-        assert_eq!(&v.bar, "POINT(25.2 54.2)");
-    }
+    // enable extension loading
+    conn.enable_load_extension(true).unwrap();
+
+    // try loading module without enabling extension loading
+    let result = sql_query("SELECT load_extension('/tmp/foo');")
+        .execute(&conn)
+        .expect_err("should have been error");
+
+    assert_ne!(&format!("{}", &result), "not authorized");
 }
