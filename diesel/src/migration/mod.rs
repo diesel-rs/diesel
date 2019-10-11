7e78674374b09cc0a26c9d25684fa8da71a1dd24
Georg Semmler <georg_semmler_05@web.de>
2019-10-11 11:17:50 +0200

    Put the create table statement into a own file

--- diesel/src/migration/mod.rs
+++ diesel/src/migration/mod.rs
@@ -57,10 +57,7 @@ impl<'a> Migration for &'a dyn Migration {
 
 /// Create table statement for the `__diesel_schema_migrations` used
 /// used by the postgresql, sqlite and mysql backend
-pub const CREATE_MIGRATIONS_TABLE: &str = "CREATE TABLE IF NOT EXISTS __diesel_schema_migrations (\
-                                           version VARCHAR(50) PRIMARY KEY NOT NULL,\
-                                           run_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP\
-                                           )";
+pub const CREATE_MIGRATIONS_TABLE: &str = include_str!("setup_migration_table.sql");
 
 /// A trait indicating that a connection could be used to manage migrations
 ///
