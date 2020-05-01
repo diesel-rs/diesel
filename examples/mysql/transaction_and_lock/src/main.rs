5c2c9cf30d61b10fb3a12dcfa0ac81fc9b4defa0
吴翱翔 <pylint@yandex.com>
2020-05-01 14:59:28 +0800

    Add mysql general_log for transaction_and_lock example

--- examples/mysql/transaction_and_lock/src/main.rs
+++ examples/mysql/transaction_and_lock/src/main.rs
@@ -1,13 +1,12 @@
 #[macro_use]
 extern crate diesel;
 
-use diesel::prelude::*;
-// use diesel::query_dsl::methods::{LockingDsl};
+use diesel::{MysqlConnection, Connection, RunQueryDsl, QueryDsl, ExpressionMethods};
 
 pub fn establish_connection() -> MysqlConnection {
     dotenv::dotenv().ok();
 
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env file");
     MysqlConnection::establish(&database_url).unwrap()
 }
 
@@ -23,7 +22,7 @@ mod schema {
 use schema::users;
 use schema::users::dsl::users as users_dsl;
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable)]
 pub struct User {
     pub id: u32,
     pub account_balance: i32
@@ -49,7 +48,6 @@ fn update_user_balance_with_lock(
         // Lock the user record to avoid modification by other threads
         users_dsl.find(user_id).for_update().execute(ref_db_connection)?;
 
-        // Error: diesel::update(users_dsl.find(user_id).with_lock())
         diesel::update(users_dsl.find(user_id))
           .set(users::account_balance.eq(new_balance))
           .execute(ref_db_connection)?;
@@ -57,6 +55,29 @@ fn update_user_balance_with_lock(
     }).unwrap();
 }
 
+/* Mysql general_log
+Connect	root@localhost on database_name using Socket
+Query	SET sql_mode=(SELECT CONCAT(@@sql_mode, ',PIPES_AS_CONCAT'))
+Query	SET time_zone = '+00:00'
+Query	SET character_set_client = 'utf8mb4'
+Query	SET character_set_connection = 'utf8mb4'
+Query	SET character_set_results = 'utf8mb4'
+Prepare	INSERT INTO `users` () VALUES ()
+Execute	INSERT INTO `users` () VALUES ()
+Close stmt
+Prepare	SELECT `users`.`id`, `users`.`account_balance` FROM `users` ORDER BY `users`.`id` DESC LIMIT ?
+Execute	SELECT `users`.`id`, `users`.`account_balance` FROM `users` ORDER BY `users`.`id` DESC LIMIT 1
+Query	BEGIN
+Prepare	SELECT `users`.`id`, `users`.`account_balance` FROM `users` WHERE `users`.`id` = ? FOR UPDATE
+Execute	SELECT `users`.`id`, `users`.`account_balance` FROM `users` WHERE `users`.`id` = 8 FOR UPDATE
+Prepare	UPDATE `users` SET `account_balance` = ? WHERE `users`.`id` = ?
+Execute	UPDATE `users` SET `account_balance` = 10 WHERE `users`.`id` = 8
+Close stmt
+Query	COMMIT
+Prepare	SELECT `users`.`id`, `users`.`account_balance` FROM `users` WHERE `users`.`id` = ? LIMIT ?
+Execute	SELECT `users`.`id`, `users`.`account_balance` FROM `users` WHERE `users`.`id` = 8 LIMIT 1
+Quit
+*/
 fn main() {
     let db_connection = establish_connection();
     let user = create_user(&db_connection);
@@ -64,5 +85,4 @@ fn main() {
     update_user_balance_with_lock(&db_connection, user.id, new_balance);
     let user_new_state: User = users_dsl.find(user.id).first(&db_connection).unwrap();
     assert_eq!(user_new_state.account_balance, new_balance);
-    dbg!(user_new_state);
 }
