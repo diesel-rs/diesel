3486fc3ef22261efb5b26f2f2d80c9fe1b233a78
Thomas BESSOU <thomas.bessou@hotmail.fr>
2020-12-19 16:25:47 +0100

    revert to simpler impl AsExpression<Nullable<ST>> for Option<T>

--- diesel/src/type_impls/option.rs
+++ diesel/src/type_impls/option.rs
@@ -60,11 +60,12 @@ where
     }
 }
 
-impl<T, ST> AsExpression<ST> for Option<T>
+impl<T, ST> AsExpression<Nullable<ST>> for Option<T>
 where
-    ST: SqlType<IsNull = is_nullable::IsNullable> + SingleValue,
+    ST: SqlType<IsNull = is_nullable::NotNull>,
+    Nullable<ST>: TypedExpressionType,
 {
-    type Expression = Bound<ST, Self>;
+    type Expression = Bound<Nullable<ST>, Self>;
 
     fn as_expression(self) -> Self::Expression {
         Bound::new(self)
