9b8b724a3a486d98089ccd1c8ecaadb70da7eb92
Georg Semmler <github@weiznich.de>
2021-01-20 18:01:03 +0100

    Fix #2203
    Custom postgres types could live in different schemas, so we need to
    incorporate the corresponding schema in the lookup request. This commit
    adds an optional parameter to `#[derive(SqlType)]` that allows to
    configure the corresponding schema. Otherwise the default search path is used.

--- diesel_tests/tests/custom_types.rs
+++ diesel_tests/tests/custom_types.rs
@@ -84,3 +84,83 @@ fn custom_types_round_trip() {
         .unwrap();
     assert_eq!(data, inserted);
 }
+
+table! {
+    use diesel::sql_types::*;
+    use super::MyTypeInCustomSchema;
+    custom_schema.custom_types_with_custom_schema {
+        id -> Integer,
+        custom_enum -> MyTypeInCustomSchema,
+    }
+}
+
+#[derive(SqlType)]
+#[postgres(type_name = "my_type", type_schema = "custom_schema")]
+pub struct MyTypeInCustomSchema;
+
+#[derive(Debug, PartialEq, FromSqlRow, AsExpression)]
+#[sql_type = "MyTypeInCustomSchema"]
+pub enum MyEnumInCustomSchema {
+    Foo,
+    Bar,
+}
+
+impl ToSql<MyTypeInCustomSchema, Pg> for MyEnumInCustomSchema {
+    fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+        match *self {
+            MyEnumInCustomSchema::Foo => out.write_all(b"foo")?,
+            MyEnumInCustomSchema::Bar => out.write_all(b"bar")?,
+        }
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<MyTypeInCustomSchema, Pg> for MyEnumInCustomSchema {
+    fn from_sql(bytes: PgValue<'_>) -> deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"foo" => Ok(MyEnumInCustomSchema::Foo),
+            b"bar" => Ok(MyEnumInCustomSchema::Bar),
+            _ => Err("Unrecognized enum variant".into()),
+        }
+    }
+}
+
+#[derive(Insertable, Queryable, Identifiable, Debug, PartialEq)]
+#[table_name = "custom_types_with_custom_schema"]
+struct HasCustomTypesInCustomSchema {
+    id: i32,
+    custom_enum: MyEnumInCustomSchema,
+}
+
+#[test]
+fn custom_types_in_custom_schema_round_trip() {
+    let data = vec![
+        HasCustomTypesInCustomSchema {
+            id: 1,
+            custom_enum: MyEnumInCustomSchema::Foo,
+        },
+        HasCustomTypesInCustomSchema {
+            id: 2,
+            custom_enum: MyEnumInCustomSchema::Bar,
+        },
+    ];
+    let connection = connection();
+    connection
+        .batch_execute(
+            r#"
+        CREATE SCHEMA IF NOT EXISTS custom_schema;
+        CREATE TYPE custom_schema.my_type AS ENUM ('foo', 'bar');
+        CREATE TABLE custom_schema.custom_types_with_custom_schema (
+            id SERIAL PRIMARY KEY,
+            custom_enum custom_schema.my_type NOT NULL
+        );
+    "#,
+        )
+        .unwrap();
+
+    let inserted = insert_into(custom_types_with_custom_schema::table)
+        .values(&data)
+        .get_results(&connection)
+        .unwrap();
+    assert_eq!(data, inserted);
+}
