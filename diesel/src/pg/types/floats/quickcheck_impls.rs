e512b366f09059a7d1a6ebf7f901332dfaa69bb4
Iban Eguia (Razican) <razican@protonmail.ch>
2021-10-23 19:01:28 +0200

    cargo fmt

--- diesel/src/pg/types/floats/quickcheck_impls.rs
+++ diesel/src/pg/types/floats/quickcheck_impls.rs
@@ -37,11 +37,7 @@ impl Arbitrary for PgNumeric {
     }
 }
 
-fn gen_vec_of_appropriate_length_valid_digits(
-    g: &mut Gen,
-    weight: u16,
-    scale: u16,
-) -> Vec<i16> {
+fn gen_vec_of_appropriate_length_valid_digits(g: &mut Gen, weight: u16, scale: u16) -> Vec<i16> {
     let max_digits = ::std::cmp::min(weight, scale);
     let mut digits = Vec::<Digit>::arbitrary(g)
         .into_iter()
