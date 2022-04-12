8dd9238fb215e89c4c719ddfea52c6dd57a3cb85
telios <me@telios.dev>
2022-04-12 06:26:05 +0000

    Fix JSON type_impl feature gates to be for the respective backends

--- diesel/src/type_impls/json.rs
+++ diesel/src/type_impls/json.rs
@@ -3,11 +3,11 @@
 use crate::deserialize::FromSqlRow;
 use crate::expression::AsExpression;
 use crate::sql_types::Json;
-#[cfg(feature = "postgres")]
+#[cfg(feature = "postgres_backend")]
 use crate::sql_types::Jsonb;
 
 #[derive(AsExpression, FromSqlRow)]
 #[diesel(foreign_derive)]
 #[diesel(sql_type = Json)]
-#[cfg_attr(feature = "postgres", diesel(sql_type = Jsonb))]
+#[cfg_attr(feature = "postgres_backend", diesel(sql_type = Jsonb))]
 struct SerdeJsonValueProxy(serde_json::Value);
