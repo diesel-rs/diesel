336e9a483ff11f94da40630df792e836781c64d2
Georg Semmler <github@weiznich.de>
2022-09-25 17:33:30 +0200

    Verify that group_by expression is valid for the given table
    This commit fixes a bug where diesel would accept group by expressions
    that contain columns of tables not even appearing in the current from
    clause. The provided compile test contains some examples that would have
    been accepted before this commit (not all of them where accepted before,
    only those with `.select(…).group_by(…)`).
    
    This is technically a breaking change as this rejects previously
    accepted queries. However I would argue that this is at the same time a
    critical bugfix as this turns invalid queries in a compiler error.

---
+++ diesel_compile_tests/tests/fail/invalid_group_by.rs
@@ -0,0 +1,76 @@
+extern crate diesel;
+
+use diesel::alias;
+use diesel::prelude::*;
+
+table! {
+    users {
+        id -> Integer,
+        name -> VarChar,
+    }
+}
+
+table! {
+    posts {
+        id -> Integer,
+    }
+}
+
+allow_columns_to_appear_in_same_group_by_clause!(users::id, posts::id);
+
+fn main() {
+    let conn = &mut PgConnection::establish("..").unwrap();
+
+    // this fails because `posts` is not part of the from clause
+    users::table
+        .group_by(posts::id)
+        .select(users::id)
+        .execute(conn)
+        .unwrap();
+
+    // order of select and group by does not matter for the error
+    users::table
+        .select(users::id)
+        .group_by(posts::id)
+        .execute(conn)
+        .unwrap();
+
+    let (user_alias, post_alias) = alias!(users as user1, posts as post1,);
+
+    // this also fails if we use aliases
+    user_alias
+        .group_by(posts::id)
+        .select(user_alias.field(users::id))
+        .execute(conn)
+        .unwrap();
+
+    users::table
+        .group_by(post_alias.field(posts::id))
+        .select(users::id)
+        .execute(conn)
+        .unwrap();
+
+    user_alias
+        .group_by(post_alias.field(posts::id))
+        .select(user_alias.field(users::id))
+        .execute(conn)
+        .unwrap();
+
+    user_alias
+        .select(user_alias.field(users::id))
+        .group_by(posts::id)
+        .execute(conn)
+        .unwrap();
+
+    users::table
+        .select(users::id)
+        .group_by(post_alias.field(posts::id))
+        .execute(conn)
+        .unwrap();
+
+    user_alias
+        .select(user_alias.field(users::id))
+        .group_by(post_alias.field(posts::id))
+        .execute(conn)
+        .unwrap();
+}
