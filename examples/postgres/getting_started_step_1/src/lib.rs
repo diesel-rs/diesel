dcec4e1c0a160a8a2c7c02811a184c3387a55eb9
Marijn Schouten <hkBst@users.noreply.github.com>
2022-10-09 15:01:15 +0200

    Replace useless call to `ok' with `unwrap'
    Replace useless call to `ok' with `unwrap'. This is also what dotenvy recommends: https://docs.rs/dotenvy/latest/dotenvy/fn.dotenv.html.

--- examples/postgres/getting_started_step_1/src/lib.rs
+++ examples/postgres/getting_started_step_1/src/lib.rs
@@ -7,7 +7,7 @@ use dotenvy::dotenv;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+    dotenv().unwrap();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
