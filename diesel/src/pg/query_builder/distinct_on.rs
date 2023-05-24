20e00a46d5362b5cb2049b3b51ebce61b7acb32a
Georg Semmler <github@weiznich.de>
2023-05-24 16:58:58 +0200

    Provide non-conflicting impls up to a tuple size of 5
    (This also uses a different strategy to generate these impls as that
    results in more maintainable code)

--- diesel/src/pg/query_builder/distinct_on.rs
+++ diesel/src/pg/query_builder/distinct_on.rs
@@ -7,7 +7,7 @@ use crate::query_builder::{
 use crate::query_dsl::methods::DistinctOnDsl;
 use crate::query_dsl::order_dsl::ValidOrderingForDistinct;
 use crate::result::QueryResult;
-use crate::{Expression, QuerySource};
+use crate::QuerySource;
 use diesel::query_builder::order_clause::OrderClause;
 
 /// Represents `DISTINCT ON (...)`
@@ -17,7 +17,56 @@ pub struct DistinctOnClause<T>(pub(crate) T);
 
 impl<T> ValidOrderingForDistinct<DistinctOnClause<T>> for NoOrderClause {}
 impl<T> ValidOrderingForDistinct<DistinctOnClause<T>> for OrderClause<(T,)> {}
-impl<T> ValidOrderingForDistinct<DistinctOnClause<T>> for OrderClause<T> where T: Expression {}
+impl<T> ValidOrderingForDistinct<DistinctOnClause<T>> for OrderClause<T> where T: crate::Column {}
+
+macro_rules! valid_ordering {
+    (@skip: ($ST1: ident, $($ST:ident,)*), $T1:ident, ) => {};
+    (@skip: ($ST1: ident, $($ST:ident,)*), $T1:ident, $($T:ident,)+) => {
+        valid_ordering!(($($ST,)*), ($ST1,), $($T,)*);
+    };
+    (($ST1: ident,), ($($OT:ident,)*), $T1:ident,) => {
+        impl<$T1, $ST1, $($OT,)*> ValidOrderingForDistinct<DistinctOnClause<($ST1, $($OT,)*)>> for OrderClause<($T1,)>
+        where $T1: crate::pg::OrderDecorator<Column = $ST1>,
+        {}
+        impl<$T1, $ST1, $($OT,)*> ValidOrderingForDistinct<DistinctOnClause<($T1,)>> for OrderClause<($ST1, $($OT,)*)>
+        where $ST1: crate::pg::OrderDecorator<Column = $T1>,
+        {}
+
+        impl<$T1, $ST1, $($OT,)*> ValidOrderingForDistinct<DistinctOnClause<$T1>> for OrderClause<($ST1, $($OT,)*)>
+        where $ST1: crate::pg::OrderDecorator<Column = $T1>,
+              $T1: crate::Column,
+        {}
+    };
+    (($ST1: ident, $($ST:ident,)*), ($($OT: ident,)*), $T1:ident, $($T:ident,)+) => {
+        impl<$T1, $($T,)* $ST1, $($ST,)* $($OT,)*> ValidOrderingForDistinct<DistinctOnClause<($ST1, $($ST,)* $($OT,)*)>> for OrderClause<($T1, $($T,)*)>
+        where $T1: crate::pg::OrderDecorator<Column = $ST1>,
+              $($T: crate::pg::OrderDecorator<Column = $ST>,)*
+        {}
+        impl<$T1, $($T,)* $ST1, $($ST,)* $($OT,)*> ValidOrderingForDistinct<DistinctOnClause<($T1, $($T,)*)>> for OrderClause<($ST1, $($ST,)* $($OT,)*)>
+        where $ST1: crate::pg::OrderDecorator<Column = $T1>,
+        $($ST: crate::pg::OrderDecorator<Column = $T>,)*
+        {}
+        valid_ordering!(($($ST,)*), ($($OT,)* $ST1,), $($T,)*);
+    };
+    ($(
+        $Tuple:tt {
+            $(($idx:tt) -> $T:ident, $ST:ident, $TT:ident,)+
+        }
+    )+) => {
+        $(
+            impl<$($T,)* $($ST,)*> ValidOrderingForDistinct<DistinctOnClause<($($T,)*)>> for OrderClause<($($ST,)*)>
+            where $($ST: crate::pg::OrderDecorator<Column = $T>,)*
+            {}
+            valid_ordering!(@skip: ($($ST,)*), $($T,)*);
+        )*
+    }
+}
+
+// we only generate these impl up to a tuple size of 5 as we generate n*n + 4 impls here
+// If we would generate these impls up to  max_table_column_count tuple elements that
+// would be a really large number for 128 tuple elements (~64k trait impls)
+// It's fine to increase this number at some point in the future gradually
+diesel_derives::__diesel_for_each_tuple!(valid_ordering, 5);
 
 /// A decorator trait for [`OrderClause`]
 /// It helps to have bounds on either Col, Asc<Col> and Desc<Col>.
