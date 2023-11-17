6f6b04d3c223f3995dbc0c7a54f8bb4a87387fed
Peter C. S. Scholtens <peter.scholtens@freedom.nl>
2023-11-17 17:11:56 +0100

    Example and step-by-step explanation added for composite-types in PostgreSQL.

---
+++ examples/postgres/composite_types/migrations/2023-10-23-111951_composite2rust_coordinates/down.sql
@@ -0,0 +1,6 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE coordinates;
+DROP FUNCTION distance_from_origin;
+DROP FUNCTION shortest_distance;
+DROP FUNCTION longest_distance;
+DROP TYPE my_comp_type;
