db070892aa7e3ca712f0c0e173a3ff734094c9f5
Drew Vogel <dvogel@github>
2024-02-03 09:51:59 -0600

    Add SQL tests for postgres tablesample

--- diesel/src/pg/query_builder/tablesample.rs
+++ diesel/src/pg/query_builder/tablesample.rs
@@ -139,3 +139,51 @@ where
         S::all_columns()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::backend::Backend;
+    use crate::pg::Pg;
+    use crate::query_builder::QueryBuilder;
+    use diesel::dsl::*;
+    use diesel::*;
+
+    // TODO: Borrowed from src/pg/transaction.rs -- should this be extracted elsewhere?
+    macro_rules! assert_sql {
+        ($query:expr, $sql:expr) => {
+            let mut query_builder = <Pg as Backend>::QueryBuilder::default();
+            $query.to_sql(&mut query_builder, &Pg).unwrap();
+            let sql = query_builder.finish();
+            assert_eq!(sql, $sql);
+        };
+    }
+
+    table! {
+        users {
+            id -> Integer,
+            name -> VarChar,
+        }
+    }
+
+    #[test]
+    fn test_generated_tablesample_sql() {
+        assert_sql!(
+            users::table.tablesample(TablesampleMethod::Bernoulli(10), TablesampleSeed::Auto),
+            "\"users\" TABLESAMPLE BERNOULLI($1)"
+        );
+
+        assert_sql!(
+            users::table.tablesample(TablesampleMethod::System(10), TablesampleSeed::Auto),
+            "\"users\" TABLESAMPLE SYSTEM($1)"
+        );
+
+        assert_sql!(
+            users::table.tablesample(
+                TablesampleMethod::System(10),
+                TablesampleSeed::Repeatable(42.0),
+            ),
+            "\"users\" TABLESAMPLE SYSTEM($1) REPEATABLE($2)"
+        );
+    }
+}
