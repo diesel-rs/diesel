3383b426fcc2d73da246ee9b120e5aa1bdea1c90
Georg Semmler <github@weiznich.de>
2024-02-06 20:19:40 +0100

    Fix a few new lint warnings about dead code

--- diesel/src/sqlite/connection/raw.rs
+++ diesel/src/sqlite/connection/raw.rs
@@ -261,7 +261,7 @@ impl Drop for RawConnection {
 enum SqliteCallbackError {
     Abort(&'static str),
     DieselError(crate::result::Error),
-    Panic(Box<dyn std::any::Any + Send>, String),
+    Panic(String),
 }
 
 impl SqliteCallbackError {
@@ -273,7 +273,7 @@ impl SqliteCallbackError {
                 s = e.to_string();
                 &s
             }
-            SqliteCallbackError::Panic(_, msg) => msg,
+            SqliteCallbackError::Panic(msg) => msg,
         };
         unsafe {
             context_error_str(ctx, msg);
@@ -347,12 +347,7 @@ extern "C" fn run_custom_function<F, Ret, RetSqlType>(
         }
         Ok(())
     })
-    .unwrap_or_else(|p| {
-        Err(SqliteCallbackError::Panic(
-            p,
-            data_ptr.function_name.clone(),
-        ))
-    });
+    .unwrap_or_else(|p| Err(SqliteCallbackError::Panic(data_ptr.function_name.clone())));
     if let Err(e) = result {
         e.emit(ctx);
     }
@@ -383,10 +378,10 @@ extern "C" fn run_aggregator_step_function<ArgsSqlType, RetSqlType, Args, Ret, A
         run_aggregator_step::<A, Args, ArgsSqlType>(ctx, args)
     })
     .unwrap_or_else(|e| {
-        Err(SqliteCallbackError::Panic(
-            e,
-            format!("{}::step() panicked", std::any::type_name::<A>()),
-        ))
+        Err(SqliteCallbackError::Panic(format!(
+            "{}::step() panicked",
+            std::any::type_name::<A>()
+        )))
     });
 
     match result {
@@ -496,11 +491,11 @@ extern "C" fn run_aggregator_final_function<ArgsSqlType, RetSqlType, Args, Ret, 
         }
         Ok(())
     })
-    .unwrap_or_else(|e| {
-        Err(SqliteCallbackError::Panic(
-            e,
-            format!("{}::finalize() panicked", std::any::type_name::<A>()),
-        ))
+    .unwrap_or_else(|_e| {
+        Err(SqliteCallbackError::Panic(format!(
+            "{}::finalize() panicked",
+            std::any::type_name::<A>()
+        )))
     });
     if let Err(e) = result {
         e.emit(ctx);
@@ -570,7 +565,6 @@ where
     })
     .unwrap_or_else(|p| {
         Err(SqliteCallbackError::Panic(
-            p,
             user_ptr
                 .map(|u| u.collation_name.clone())
                 .unwrap_or_default(),
@@ -601,7 +595,7 @@ where
             );
             std::process::abort()
         }
-        Err(SqliteCallbackError::Panic(_, msg)) => {
+        Err(SqliteCallbackError::Panic(msg)) => {
             eprintln!("Collation function {} panicked", msg);
             std::process::abort()
         }
