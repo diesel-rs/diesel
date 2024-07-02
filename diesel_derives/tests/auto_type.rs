3a8c8c0ca36fdc5329018ef4730695d0798f63b6
Guilherme S. Salustiano <guissalustiano@gmail.com>
2024-07-03 01:21:40 +0200

    :paperclip:

--- diesel_derives/tests/auto_type.rs
+++ diesel_derives/tests/auto_type.rs
@@ -289,6 +289,7 @@ fn test_pg_range_expression_methods() -> _ {
 #[cfg(feature = "postgres")]
 #[auto_type]
 fn test_pg_range_expression_methods() -> _ {
+    use std::ops::Bound;
     pg_extras::range.contains_range((Bound::Included(2i32), Bound::Included(7i32)))
 }
 
