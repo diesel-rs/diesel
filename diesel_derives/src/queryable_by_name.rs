00a901664c9f1a69466af15285f4b9aab70c6de7
Georg Semmler <github@weiznich.de>
2024-08-02 06:30:53 +0000

    Merge pull request #4132 from fiadliel/same_rust_type_multiple_db_types
    Specify type of diesel::row::NamedRow::get in QueryableByName macro.

--- diesel_derives/src/queryable_by_name.rs
+++ diesel_derives/src/queryable_by_name.rs
@@ -23,12 +23,13 @@ pub fn derive(item: DeriveInput) -> Result<TokenStream> {
             if f.embed() {
                 Ok(quote!(<#field_ty as QueryableByName<__DB>>::build(row)?))
             } else {
+                let st = sql_type(f, &model)?;
                 let deserialize_ty = f.ty_for_deserialize();
                 let name = f.column_name()?;
                 let name = LitStr::new(&name.to_string(), name.span());
                 Ok(quote!(
                    {
-                       let field = diesel::row::NamedRow::get(row, #name)?;
+                       let field = diesel::row::NamedRow::get::<#st, #deserialize_ty>(row, #name)?;
                        <#deserialize_ty as Into<#field_ty>>::into(field)
                    }
                 ))
