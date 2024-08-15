5b01b942aa80d630a007f823ab8010bf00883cce
Marvin Hansen <marvin.hansen@gmail.com>
2024-08-16 06:57:39 +0800

    Fixed tests in custom array example.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- examples/postgres/custom_arrays/src/schema.rs
+++ examples/postgres/custom_arrays/src/schema.rs
@@ -3,7 +3,7 @@
 pub mod smdb {
     pub mod sql_types {
         #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
-        #[diesel(postgres_type(name = "service_endpoint", schema = "smdb"))]
+        #[diesel(postgres_type(name = "service_endpoint"))]
         pub struct ServiceEndpoint;
     }
 
