c7fbf34f8da454351cff1d462979f67f8387d010
Marvin Hansen <marvin.hansen@gmail.com>
2024-08-16 06:57:39 +0800

    Fixed tests in custom array example.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- examples/postgres/custom_arrays/migrations/2024-08-15-070500_services/down.sql
+++ examples/postgres/custom_arrays/migrations/2024-08-15-070500_services/down.sql
@@ -1,5 +1,5 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE IF EXISTS smdb.service;
-DROP TYPE IF EXISTS smdb.service_endpoint CASCADE;
-DROP TYPE IF EXISTS smdb.protocol_type CASCADE;
+DROP TYPE IF EXISTS service_endpoint CASCADE;
+DROP TYPE IF EXISTS protocol_type CASCADE;
 DROP schema IF EXISTS smdb;
