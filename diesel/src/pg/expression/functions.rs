fec9e43e9336d5226b87564254249218b9d3e057
edwinlzs <edwinlzs97@gmail.com>
2024-08-21 21:15:58 +0800

    fix: fix array_remove return type

--- diesel/src/pg/expression/functions.rs
+++ diesel/src/pg/expression/functions.rs
@@ -850,18 +850,18 @@ define_sql_function! {
     /// #     use diesel::sql_types::{Nullable, Integer, Array};
     /// #     let connection = &mut establish_connection();
     /// let ints = diesel::select(array_remove::<Array<_>, Integer, _, _>(vec![1, 2, 3, 2], 2))
-    ///     .get_result::<Option<Vec<i32>>>(connection)?;
-    /// assert_eq!(Some(vec![1, 3]), ints);
-
+    ///     .get_result::<Vec<i32>>(connection)?;
+    /// assert_eq!(vec![1, 3], ints);
+    ///
     /// let ints = diesel::select(array_remove::<Array<_>, Nullable<Integer>, _, _>(vec![None, Some(1), Some(2), None, Some(4)], None::<i32>))
-    ///     .get_result::<Option<Vec<Option<i32>>>>(connection)?;
-    /// assert_eq!(Some(vec![Some(1), Some(2), Some(4)]), ints);
-
+    ///     .get_result::<Vec<Option<i32>>>(connection)?;
+    /// assert_eq!(vec![Some(1), Some(2), Some(4)], ints);
+    ///
     /// let ints = diesel::select(array_remove::<Nullable<Array<_>>, Nullable<Integer>, _, _>(None::<Vec<i32>>, None::<i32>))
     ///     .get_result::<Option<Vec<Option<i32>>>>(connection)?;
     /// assert_eq!(None, ints);
     /// #     Ok(())
     /// # }
     /// ```
-    fn array_remove<Arr: ArrayOrNullableArray<Inner=T> + SingleValue, T: SingleValue>(a: Arr, e: T) -> Nullable<Array<T>>;
+    fn array_remove<Arr: ArrayOrNullableArray<Inner=T> + SingleValue, T: SingleValue>(a: Arr, e: T) -> Arr;
 }
