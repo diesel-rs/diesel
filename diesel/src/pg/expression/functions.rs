b82895044d8f141e46a292a995603e057c59d0ed
Ali Tariq <ali.tariq@A006-00964.local>
2024-08-23 13:44:51 +0500

    fixed doc test and added new doc tests for None

--- diesel/src/pg/expression/functions.rs
+++ diesel/src/pg/expression/functions.rs
@@ -803,16 +803,19 @@ define_sql_function! {
     /// #   use diesel::sql_types::{Nullable,Array,Integer};
     /// #   let connection = &mut establish_connection();
     ///
-    /// let dims = diesel::select(array_dims::<Array<_>,_,_>(vec![1,2]))
-    ///     .get_result::<String>(connection).unwrap();
+    /// let dims = diesel::select(array_dims::<Array<Integer>,_>(vec![1,2]))
+    ///     .get_result::<String>(connection)?;
     /// assert!(String::from("[1:2]").eq(&dims));
     ///
-    /// let dims = diesel::select(array_dims::<Array<_>,_,_>(vec![vec![1,2,3],vec![4,5,6]]))
-    /// .get_result::<String>(connection).unwrap();
-    /// assert!(String::from("[1:2][1:3]").eq(&dims));
+    /// let dims = diesel::select(array_dims::<Array<Integer>,_>(vec![None::<i32>,Some(2)]))
+    ///     .get_result::<String>(connection)?;
+    /// assert!(String::from("[1:2]").eq(&dims));
     ///
+    /// let dims = diesel::select(array_dims::<Array<Integer>,_>(vec![None::<i32>]))
+    ///     .get_result::<String>(connection)?;
+    /// assert!(String::from("[1:1]").eq(&dims));
     /// # Ok(())
     /// # }
     ///
-    fn array_dims<Arr:ArrayOrNullableArray<Inner=T> + SingleValue,T:SingleValue>(arr:Arr) -> Text;
+    fn array_dims<Arr:ArrayOrNullableArray<> + SingleValue>(arr:Arr) -> Text;
 }
