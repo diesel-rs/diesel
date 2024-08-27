f9170b2ac673db80c9d3fe40b7113ce9266b73ac
wowinter13 <vla-dy@yandex.ru>
2024-08-27 02:18:22 +0200

    feat: implement array_lower function

--- diesel_derives/tests/auto_type.rs
+++ diesel_derives/tests/auto_type.rs
@@ -426,6 +426,7 @@ fn postgres_functions() -> _ {
         array_length(pg_extras::array, 1_i32),
         array_fill(pg_extras::id, pg_extras::array),
         array_fill_with_lower_bound(pg_extras::id, pg_extras::array, pg_extras::array),
+        array_lower(pg_extras::array, 1_i32),
     )
 }
 
