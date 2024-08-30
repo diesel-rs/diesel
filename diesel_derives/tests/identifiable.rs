62496e475c12149b8aabc6b1be3d0e319bad816e
Geoffroy Jaffa Planquart <geoffroy@planquart.fr>
2024-08-30 11:27:20 +0200

    Add test case for non-copy primary key that rely on serialize_as

--- diesel_derives/tests/identifiable.rs
+++ diesel_derives/tests/identifiable.rs
@@ -173,3 +173,26 @@ fn derive_identifiable_with_pk_serialize_as() {
     assert_eq!(MyI32(1), foo1.id());
     assert_eq!(MyI32(2), foo2.id());
 }
+
+#[test]
+fn derive_identifiable_with_non_copy_pk_serialize() {
+    #[derive(Debug, PartialEq, Eq, Hash)]
+    struct MyString(String);
+
+    impl From<String> for MyString {
+        fn from(value: String) -> Self {
+            MyString(value)
+        }
+    }
+
+    #[derive(Identifiable)]
+    struct Foo {
+        #[diesel(serialize_as = MyString)]
+        id: String,
+    }
+
+    let foo1 = Foo { id: "1".to_owned() };
+    let foo2 = Foo { id: "2".to_owned() };
+    assert_eq!(MyString("1".to_owned()), foo1.id());
+    assert_eq!(MyString("2".to_owned()), foo2.id());
+}
