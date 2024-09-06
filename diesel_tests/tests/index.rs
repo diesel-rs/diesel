ce0630f7b8be6a99e7b57641cc69b93c1e01169a
Georg Semmler <github@weiznich.de>
2024-09-06 11:31:33 +0200

    Fix imports + rustfmt

--- diesel_tests/tests/index.rs
+++ diesel_tests/tests/index.rs
@@ -1,14 +1,10 @@
-use crate::schema::connection;
-use crate::schema::*;
-
-
 #[cfg(feature = "postgres")]
 #[test]
 fn test_array_index() {
-    use diesel::{PgArrayExpressionMethods, RunQueryDsl};
     use diesel::dsl::array_append;
-    use diesel::sql_types::{Array,Integer};
-    let connection = &mut connection();
+    use diesel::sql_types::{Array, Integer};
+    use diesel::{PgArrayExpressionMethods, RunQueryDsl};
+    let connection = &mut crate::schema::connection();
     let result = diesel::select(array_append::<Array<_>, Integer, _, _>(vec![1, 2, 3], 4).index(4))
         .get_result::<i32>(connection)
         .unwrap();
