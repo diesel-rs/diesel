0f0d9a60704e972da876908cc858e3619b159315
Jean-Marc Le Roux <contact@jmlx.io>
2024-09-25 18:42:02 +0200

    fix broken ref to doctest_setup.rs

--- diesel/src/sql_types/mod.rs
+++ diesel/src/sql_types/mod.rs
@@ -472,7 +472,7 @@ pub struct Json;
 ///
 /// ```rust
 /// # #![allow(dead_code)]
-/// # include!("../../doctest_setup.rs");
+/// # include!("../doctest_setup.rs");
 /// #
 /// table! {
 ///     contacts {
