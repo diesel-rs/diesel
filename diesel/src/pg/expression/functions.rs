561ca49bea2c3d4e925ea8b448c28a34388c2050
ali.tariq <ali.tariq@arbisoft.com>
2024-11-05 12:11:25 +0500

    implement jsonb_path_exists

--- diesel/src/pg/expression/functions.rs
+++ diesel/src/pg/expression/functions.rs
@@ -2281,6 +2281,31 @@ define_sql_function! {
 }
 
 #[cfg(feature = "postgres_backend")]
-define_sql_function!{
-    fn jsonb_path_exists<J: JsonbOrNullableJsonb + SingleValue, >(jsonb:J,path:Jsonb);
+define_sql_function! {
+    /// This form of jsonb_object takes keys and values pairwise from two separate arrays.
+    /// In all other respects it is identical to the one-argument form.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # include!("../../doctest_setup.rs");
+    /// #
+    /// # fn main() {
+    /// #     run_test().unwrap();
+    /// # }
+    /// #
+    /// # fn run_test() -> QueryResult<()> {
+    /// #     use diesel::dsl::{jsonb_path_exists};
+    /// #     use diesel::sql_types::{Array, Nullable, Text,Jsonb, Jsonpath};
+    /// #     use serde_json::Value;
+    /// #     use jsonpath_rust::JsonPath;
+    /// #     let connection = &mut establish_connection();
+    /// let jsonb:Value = serde_json::json!({"a":[1,2,3,4,5]});
+    /// let json_path = jsonpath_rust::path!("$.a[*] ? (@ >= 2 && @ <= 4)");
+    /// let exists = jsonb_path_exists::<Jsonb,Jsonpath,_,_>(jsonb,json_path);
+    /// assert!(exists);
+    /// #     Ok(())
+    /// # }
+    /// ```
+    fn jsonb_path_exists<J: JsonbOrNullableJsonb + SingleValue,P:MaybeNullableValue<Jsonpath>>(jsonb:J,path:P)->Bool;
 }
--- diesel/src/pg/types/json_path.rs
+++ diesel/src/pg/types/json_path.rs
@@ -1,43 +1,54 @@
+use crate::deserialize::FromSql;
+use crate::pg::{backend::Pg, PgValue};
+use crate::serialize::{IsNull, Output, ToSql};
+use crate::{deserialize, serialize, sql_types};
+use std::error::Error;
 use std::fmt::{Debug, Display};
 use std::io::Write;
 use std::str;
 use std::str::FromStr;
-use crate::{deserialize, serialize, sql_types};
-use crate::deserialize::FromSql;
-use crate::pg::Pg;
-use crate::serialize::{Output, ToSql};
 
-#[cfg(all(feature = "postgres_backend", feature = "serde_json"))]
 impl FromSql<sql_types::Jsonpath, Pg> for jsonpath_rust::JsonPath {
-    fn from_sql(value: Pg::RawValue<'_>) -> deserialize::Result<Self> {
-        let str = str::from_utf8(value.as_bytes()).map_err(|_| "Invalid path".into())?;
+    fn from_sql(value: PgValue<'_>) -> deserialize::Result<Self> {
+        println!("{:?}", value.as_bytes());
+        let str = str::from_utf8(value.as_bytes())
+            .map_err(|_| <&str as Into<Box<dyn Error + Send + Sync>>>::into("Invalid path"))?;
+        println!("{}", str);
         jsonpath_rust::JsonPath::from_str(str).map_err(|_| "Invalid path".into())
     }
 }
 
 impl ToSql<sql_types::Jsonpath, Pg> for jsonpath_rust::JsonPath {
     fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
-        
+        println!("{}", self);
+        out.write_all(self.to_string().as_bytes())
+            .map(|_| IsNull::No)
+            .map_err(Into::into)
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
-    use jsonpath_rust::JsonPath;
-
-    use crate::pg::Pg;
+    use super::*;
+    use crate::pg::PgValue;
     use crate::query_builder::bind_collector::ByteWrapper;
-    use crate::serialize::{Output, ToSql};
-    use crate::sql_types;
+    use jsonpath_rust::{path, JsonPath};
 
     #[test]
     fn json_to_sql() {
         let mut buffer = Vec::new();
         let mut bytes = Output::test(ByteWrapper(&mut buffer));
-        let test_json_path = jsonpath_rust::path!("$.abc");
+        let test_json_path = JsonPath::from_str("$.a[?(@ >= 2 && @ <= 4)]").unwrap();
         ToSql::<sql_types::Jsonpath, Pg>::to_sql(&test_json_path, &mut bytes).unwrap();
-        assert_eq!(buffer, b"$.abc");
+        println!("{}",String::from_utf8(buffer.clone()).unwrap());
+        assert_eq!(buffer, b"$.'a'[?(@ >= 2 && @ <= 4)]");
+    }
+
+    #[test]
+    fn path_from_sql() {
+        let path = b"$.abc";
+        let path: JsonPath =
+            FromSql::<sql_types::Jsonpath, Pg>::from_sql(PgValue::for_test(path)).unwrap();
+        assert_eq!(path, JsonPath::from_str("$.abc").unwrap());
     }
 }
