f9d768fbcdc2813b53c9c5df2f580b275b7697b7
Thomas BESSOU <thomas.bessou@hotmail.fr>
2024-11-20 21:25:01 +0100

    minor doc improvement

--- diesel/src/pg/expression/array.rs
+++ diesel/src/pg/expression/array.rs
@@ -80,7 +80,7 @@ pub trait IntoArrayExpression<ST: SqlType + TypedExpressionType> {
     fn into_array_expression(self) -> Self::ArrayExpression;
 }
 
-/// Implement as a no-op for things that were already arrays (that is, don't wrap in ARRAY[]).
+/// Implement as a no-op for expressions that were already arrays (that is, don't wrap in ARRAY[]).
 impl<ST, T> IntoArrayExpression<ST> for T
 where
     T: AsExpression<sql_types::Array<ST>>,
