a4313c26081e60550b358427d9ea23fade262333
Dhiren Serai <dhirensr@gmail.com>
2025-01-30 23:33:20 +0000

    feat: Add Sqllite json_valid
    Adding json_valid helper for sqllite.

--- diesel_derives/tests/auto_type.rs
+++ diesel_derives/tests/auto_type.rs
@@ -511,6 +511,8 @@ fn sqlite_functions() -> _ {
         json_pretty(sqlite_extras::jsonb),
         json_pretty_with_indentation(sqlite_extras::json, "  "),
         json_pretty_with_indentation(sqlite_extras::jsonb, "  "),
+        json_valid(sqlite_extras::jsonb),
+        json_valid(sqlite_extras::json),
     )
 }
 
