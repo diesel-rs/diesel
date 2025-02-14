128ffba75937259965fd953f6a0ba74798d2b2fe
Georg Semmler <github@weiznich.de>
2025-02-14 10:26:50 +0100

    Use the new `#[declare_sql_function]` macro instead

--- diesel/src/sqlite/expression/functions.rs
+++ diesel/src/sqlite/expression/functions.rs
@@ -420,10 +420,7 @@ extern "SQL" {
         j: J,
         indentation: Nullable<Text>,
     ) -> J::Out;
-}
 
-#[cfg(feature = "sqlite")]
-define_sql_function! {
     /// Returns  `true`  if the argument is well-formed JSON, or returns  `false`  if is not well-formed.
     ///
     /// # Example
@@ -478,5 +475,8 @@ define_sql_function! {
     /// # }
     /// ```
     #[sql_name = "json_valid"]
-    fn json_valid<J: JsonOrNullableJsonOrJsonbOrNullableJsonb + MaybeNullableValue<Bool>>(j: J) -> J::Out;
+    #[cfg(feature = "sqlite")]
+    fn json_valid<J: JsonOrNullableJsonOrJsonbOrNullableJsonb + MaybeNullableValue<Bool>>(
+        j: J,
+    ) -> J::Out;
 }
