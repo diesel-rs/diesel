c1a7731f8331104156112fe43dadd2a0401853a8
Ed Cradock <163921+edonsec@users.noreply.github.com>
2025-02-28 14:48:18 +0000

    Main thing is an error is thrown. This check accomodates this across different conditions (i.e. Database does not exist, or password not present).

--- diesel_cli/tests/migration_run.rs
+++ diesel_cli/tests/migration_run.rs
@@ -166,7 +166,7 @@ fn error_migrations_when_use_invalid_database_url() {
     assert!(!result.is_success());
     assert!(result
         .stderr()
-        .contains("Could not connect to database: connection to server at \"localhost\" (::1), port 5432 failed: FATAL:  database \"lemmy\" does not exist"));
+        .contains("Could not connect to database:"));
 }
 
 #[test]
