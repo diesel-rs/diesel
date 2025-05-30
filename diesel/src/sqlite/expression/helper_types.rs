e817ad73a387db1ac298c81374c6f8108c63c166
mertwole <mertwole@gmail.com>
2025-05-30 14:47:12 +0200

    fix(diesel, diesel_derives): Fix review remarks

--- diesel/src/sqlite/expression/helper_types.rs
+++ diesel/src/sqlite/expression/helper_types.rs
@@ -7,4 +7,5 @@ pub type Is<Lhs, Rhs> = Grouped<super::operators::Is<Lhs, AsExpr<Rhs, Lhs>>>;
 /// The return type of `lhs.is_not(rhs)`.
 pub type IsNot<Lhs, Rhs> = Grouped<super::operators::IsNot<Lhs, AsExpr<Rhs, Lhs>>>;
 
+#[doc(inline)]
 pub use super::functions::return_type_helpers::*;
