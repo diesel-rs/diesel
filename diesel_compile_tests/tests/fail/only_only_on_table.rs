a51f9693cfe4e48e4159238f986e9f80179c183b
Georg Semmler <github@weiznich.de>
2025-08-07 19:28:17 +0200

    Bump to rust 1.89
    Only the version bump this time as there are no new clippy warnings
    :tada:

--- diesel_compile_tests/tests/fail/only_only_on_table.rs
+++ diesel_compile_tests/tests/fail/only_only_on_table.rs
@@ -15,7 +15,7 @@ fn main() {
     foo::table.select(foo::id).filter(foo::id.eq(1)).only();
     //~^ ERROR: the method `only` exists for struct `SelectStatement<FromClause<table>, SelectClause<id>, ..., ...>`, but its trait bounds were not satisfied
     foo::table.select(foo::id.only());
-    //~^ ERROR: the method `only` exists for struct `id`, but its trait bounds were not satisfied
+    //~^ ERROR: the method `only` exists for struct `columns::id`, but its trait bounds were not satisfied
 
     // .only() is not supported for SQLite
     let mut conn = SqliteConnection::establish("").unwrap();
