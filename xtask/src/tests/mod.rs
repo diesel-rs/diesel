3674edca88532585f9e71f454fa34940c456e6f0
Georg Semmler <github@weiznich.de>
2026-01-19 12:36:57 +0100

    Add more migration sources
    This PR adds more migration sources to diesel_migrations with the goal
    to make it easier to write different kinds of migrations in multi-crate
    deployments.
    
    It adds a CombinedMigrations source that is essentially just a list of
    sources. The resulting list of migrations is just all migrations from
    all sources. That is useful for multi-crate projects as then each
    sub-project can have their own set of migrations (by using files or
    embedded migrations etc) and then combine them into a final list using
    this migration source.
    
    As a second variant a RustMigrationSource is added. The goal there is to
    make it much easier to register rust based migrations by having some
    sort of typed interface for this. This migration source needs to know
    the connection type, not only the backend. This information is then used
    to internally receive the right connection type from the migration
    connection for the user. The user has the ability to provide different
    kinds of rust structs/functions all "migration. This includes closures,
    functions, a migration builder (that in turn accepts clousures but
    allows more fine grained configuration) or even custom types (via the
    provided trait).

--- xtask/src/tests/mod.rs
+++ xtask/src/tests/mod.rs
@@ -161,6 +161,8 @@ impl TestArgs {
                 .arg(format!("diesel_tests/{backend}"))
                 .arg("-F")
                 .arg(format!("diesel-dynamic-schema/{backend}"))
+                .arg("-F")
+                .arg(format!("diesel_migrations/{backend}"))
                 .args(&self.flags);
 
             if matches!(self.backend, Backend::Mysql) {
@@ -223,7 +225,9 @@ impl TestArgs {
                 .arg("-F")
                 .arg(format!("diesel_derives/{backend}"))
                 .arg("-F")
-                .arg(format!("diesel-dynamic-schema/{backend}"));
+                .arg(format!("diesel-dynamic-schema/{backend}"))
+                .arg("-F")
+                .arg(format!("diesel_migrations/{backend}"));
             if matches!(backend, Backend::Mysql) {
                 // cannot run mysql tests in parallel
                 command.args(["-j", "1"]);
