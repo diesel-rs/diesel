41aa61134a5d922fefd4708e8c9f375c9ee72467
Georg Semmler <github@weiznich.de>
2026-01-23 09:29:34 +0100

    Bump rust to 1.93, fix a single new clippy warning and update the compile test output

--- diesel_compile_tests/tests/fail/select_for_update_cannot_be_mixed_with_some_clauses.rs
+++ diesel_compile_tests/tests/fail/select_for_update_cannot_be_mixed_with_some_clauses.rs
@@ -33,7 +33,7 @@ fn main() {
     //~| ERROR: the trait bound `SelectStatement<FromClause<...>>: GroupByDsl<_>` is not satisfied
     //~| ERROR: the trait bound `SelectStatement<..., ..., ..., ..., ..., ..., ..., ..., ...>: GroupByDsl<_>` is not satisfied
     users.group_by(id).for_update();
-    //~^ ERROR: the trait bound `SelectStatement<FromClause<...>, ..., ..., ..., ..., ..., ...>: Table` is not satisfied
+    //~^ ERROR: the trait bound `SelectStatement<FromClause<table>, ..., ..., ..., ..., ..., ...>: Table` is not satisfied
     //~| ERROR: the trait bound `SelectStatement<FromClause<...>>: LockingDsl<...>` is not satisfied
     //~| ERROR: the trait bound `SelectStatement<..., ..., ..., ..., ..., ..., ...>: LockingDsl<...>` is not satisfied
     users.into_boxed().for_update();
