481ff0a2cc68b3307cb5abbcba66d572a3f0ee25
Luca <cappelletti.luca94@gmail.com>
2026-01-27 21:02:02 +0100

    Fix print_schema_allows_none test for MySQL by using explicit FOREIGN KEY syntax

--- diesel_cli/tests/print_schema/print_schema_allows_none/mysql/schema.sql
+++ diesel_cli/tests/print_schema/print_schema_allows_none/mysql/schema.sql
@@ -1,11 +1,11 @@
 -- Three related tables.
 CREATE TABLE users (id INT PRIMARY KEY);
-CREATE TABLE posts (id INT PRIMARY KEY, user_id INT NOT NULL REFERENCES users(id));
-CREATE TABLE comments (id INT PRIMARY KEY, post_id INT NOT NULL REFERENCES posts(id));
+CREATE TABLE posts (id INT PRIMARY KEY, user_id INT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id));
+CREATE TABLE comments (id INT PRIMARY KEY, post_id INT NOT NULL, FOREIGN KEY (post_id) REFERENCES posts(id));
 
 -- Two related tables.
 CREATE TABLE sessions (id INT PRIMARY KEY);
-CREATE TABLE transactions (id INT PRIMARY KEY, session_id INT NOT NULL REFERENCES sessions(id));
+CREATE TABLE transactions (id INT PRIMARY KEY, session_id INT NOT NULL, FOREIGN KEY (session_id) REFERENCES sessions(id));
 
 -- Unrelated tables.
 CREATE TABLE cars (id INT PRIMARY KEY);
