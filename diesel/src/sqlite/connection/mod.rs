f61f2b3ea4f504719a6c43f3108be263a39963b7
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-02-12 23:00:31 +0100

    Resolved code smell

--- diesel/src/sqlite/connection/mod.rs
+++ diesel/src/sqlite/connection/mod.rs
@@ -1635,10 +1635,10 @@ impl SqliteConnection {
         F: FnMut(&str) + Send + 'static,
     {
         self.on_trace(SqliteTraceFlags::STMT, move |event| {
-            if let SqliteTraceEvent::Statement { sql, readonly } = event {
-                if readonly {
-                    hook(sql);
-                }
+            if let SqliteTraceEvent::Statement { sql, readonly } = event
+                && readonly
+            {
+                hook(sql);
             }
         });
     }
