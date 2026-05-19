7aa740a19b36bb7967f68d4119a2a97cc7010d0f
Georg Semmler <github@weiznich.de>
2026-05-19 22:01:58 +0200

    Fix possible rust code injection via `diesel print-schema`
    For special database side names for tables, types or columns we could
    have ended up generating rust code that "escapes" the generated type
    names. To prevent this from happening we now detect such cases and use
    raw string literales with an appropiated number of `#` there to escape
    the values.
    
    This commit also introduces some testing whether the compiled schemas
    compile at all. This uncovered two other issues. We could end up with
    the same table + column name. Also one of the tests for SQLite used an
    invalid schema, which this fixes as well.

---
+++ diesel_cli/tests/print_schema/print_schema_rust_injection/postgres/schema.sql
@@ -0,0 +1,8 @@
+
+CREATE TYPE "x""))] } include!(env!(""P"")); mod y { #[diesel(postgres_type(name=""z" AS ENUM ('foo', 'bar');
+
+CREATE TABLE tests(
+       id INTEGER PRIMARY KEY NOT NULL,
+       "x""] } include!(env!(""P"")); mod y { #[sql_name=""z" TEXT NOT NULL,
+       enum "x""))] } include!(env!(""P"")); mod y { #[diesel(postgres_type(name=""z" NOT NULL
+);
