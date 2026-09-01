caf515ee06de34f2fc59579355aecb17907e3521
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-09-01 10:01:07 +0200

    Expose group-by clause types for third-party backends

--- diesel/src/query_builder/group_by_clause.rs
+++ diesel/src/query_builder/group_by_clause.rs
@@ -1,4 +1,31 @@
-simple_clause!(NoGroupByClause, GroupByClause, " GROUP BY ");
+simple_clause!(
+    /// A query node indicating that no group-by clause is set.
+    NoGroupByClause,
+    /// A query node containing a group-by clause.
+    ///
+    /// ```
+    /// # #[cfg(feature = "i-implement-a-third-party-backend-and-opt-into-breaking-changes")]
+    /// # fn main() {
+    /// use diesel::query_builder::{GroupByClause, NoGroupByClause, SelectStatement};
+    ///
+    /// trait SelectionMarker {}
+    ///
+    /// impl<F, S, D, W, O, LOf, H, LC> SelectionMarker
+    ///     for SelectStatement<F, S, D, W, O, LOf, NoGroupByClause, H, LC>
+    /// {
+    /// }
+    ///
+    /// impl<F, S, D, W, O, LOf, GB, H, LC> SelectionMarker
+    ///     for SelectStatement<F, S, D, W, O, LOf, GroupByClause<GB>, H, LC>
+    /// {
+    /// }
+    /// # }
+    /// # #[cfg(not(feature = "i-implement-a-third-party-backend-and-opt-into-breaking-changes"))]
+    /// # fn main() {}
+    /// ```
+    GroupByClause,
+    " GROUP BY "
+);
 
 pub trait ValidGroupByClause {
     type Expressions;
