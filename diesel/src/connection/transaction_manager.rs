3def211c053394ba971aab25c036e9e2f311b1ac
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-09-23 12:45:58 +0200

    Drop the test-only cfg on set_requires_rollback_maybe_up_to_top_level

--- diesel/src/connection/transaction_manager.rs
+++ diesel/src/connection/transaction_manager.rs
@@ -140,8 +140,7 @@ impl TransactionManagerStatus {
         feature = "i-implement-a-third-party-backend-and-opt-into-breaking-changes",
         feature = "postgres",
         feature = "mysql",
-        feature = "mariadb",
-        test
+        feature = "mariadb"
     ))]
     #[diesel_derives::__diesel_public_if(
         feature = "i-implement-a-third-party-backend-and-opt-into-breaking-changes"
